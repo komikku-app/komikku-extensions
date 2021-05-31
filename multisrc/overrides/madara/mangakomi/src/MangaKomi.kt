601f7c38b7c488f804c63d23b5773b2d81934022
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-05-31 08:18:04 +0800

    add some more icons (#7312)
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete web_hi_res_512.png
    
    * MangaTuli: use square-shaped icon
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete web_hi_res_512.png
    
    * Yaoi Manga: use square-shaped icon
    
    * add Manga Clash icon
    
    * Create MangaKomi.kt
    
    * add MangaKomi icon
    
    * Update MadaraGenerator.kt
    
    * Create additional.gradle.kts

---
+++ multisrc/overrides/madara/mangakomi/src/MangaKomi.kt
@@ -0,0 +1,23 @@
+package eu.kanade.tachiyomi.extension.en.mangakomi
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+class MangaKomi : Madara(
+    "MangaKomi",
+    "https://mangakomi.com",
+    "en",
+    dateFormat = SimpleDateFormat("MM/dd/yy", Locale.US)
+) {
+    private val rateLimitInterceptor = RateLimitInterceptor(1)
+
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addNetworkInterceptor(rateLimitInterceptor)
+        .build()
+}
