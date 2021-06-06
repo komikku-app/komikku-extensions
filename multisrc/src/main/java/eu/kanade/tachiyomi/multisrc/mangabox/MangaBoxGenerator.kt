38013ecd52579bed72699e26200f056c306c2f0b
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-06-07 06:11:25 +0800

    add MangaLazy to madara (#7501)
    * Create MangaLazy.kt
    
    * add icon
    
    * Update MadaraGenerator.kt
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete ic_launcher.png
    
    * Delete web_hi_res_512.png
    
    * replace icon
    
    * Update MangaBoxGenerator.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangabox/MangaBoxGenerator.kt
@@ -13,7 +13,7 @@ class MangaBoxGenerator : ThemeSourceGenerator {
 
     override val sources = listOf(
         SingleLang("Mangakakalot", "https://mangakakalot.com", "en", overrideVersionCode = 2),
-        SingleLang("Manganato", "https://manganato.com", "en", overrideVersionCode = 1, pkgName = "manganelo"),
+        SingleLang("Manganato", "https://manganato.com", "en", overrideVersionCode = 2, pkgName = "manganelo"),
         SingleLang("Mangabat", "https://m.mangabat.com", "en", overrideVersionCode = 4),
         SingleLang("Mangakakalots (unoriginal)", "https://mangakakalots.com", "en", className = "Mangakakalots", pkgName = "mangakakalots"),
         SingleLang("Mangairo", "https://h.mangairo.com", "en", overrideVersionCode = 3),
