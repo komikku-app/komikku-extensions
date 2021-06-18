550bb4b5f6520951878dcca7911f42adcb6ecc57
FourTOne5 <59261191+FourTOne5@users.noreply.github.com>
2021-06-18 09:52:33 +0000

    MangaFast Fix Incorrect SimpleDateFormat Pattern (#7720)
    * Update build.gradle
    
    * Update MangaFast.kt

--- src/en/mangafast/src/eu/kanade/tachiyomi/extension/en/mangafast/MangaFast.kt
+++ src/en/mangafast/src/eu/kanade/tachiyomi/extension/en/mangafast/MangaFast.kt
@@ -104,7 +104,7 @@ class MangaFast : ParsedHttpSource() {
 
     companion object {
         val dateFormat by lazy {
-            SimpleDateFormat("MM/dd/YYYY", Locale.US)
+            SimpleDateFormat("MM/dd/yyyy", Locale.US)
         }
     }
 
