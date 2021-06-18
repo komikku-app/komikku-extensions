78bdfe5385c209c46b9c5ae6158b65b9cd238539
h-hyuuga <83582211+h-hyuuga@users.noreply.github.com>
2021-06-17 20:59:05 -0400

    Flame Scans: Harsher Rate Limit + Add Tachiyomi Identifier to User Agent (#7714)

--- multisrc/overrides/wpmangareader/flamescans/src/FlameScans.kt
+++ multisrc/overrides/wpmangareader/flamescans/src/FlameScans.kt
@@ -2,11 +2,15 @@ package eu.kanade.tachiyomi.extension.en.flamescans
 
 import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
 import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
+import okhttp3.Headers
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
 class FlameScans : WPMangaReader("Flame Scans", "https://flamescans.org", "en", "/series") {
-    private val rateLimitInterceptor = RateLimitInterceptor(2)
+    private val rateLimitInterceptor = RateLimitInterceptor(1)
+    private val userAgent = "Tachiyomi Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+    override fun headersBuilder(): Headers.Builder = Headers.Builder()
+        .add("User-Agent", userAgent)
 
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
         .connectTimeout(10, TimeUnit.SECONDS)
@@ -14,3 +18,4 @@ class FlameScans : WPMangaReader("Flame Scans", "https://flamescans.org", "en", 
         .addNetworkInterceptor(rateLimitInterceptor)
         .build()
 }
+
