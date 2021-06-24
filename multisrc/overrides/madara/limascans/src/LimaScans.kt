619f9986a6fb928986f1d99765fcf0a7cc18513c
Alessandro Jean <alessandrojean@gmail.com>
2021-06-24 10:00:40 -0300

    Add two new sources to Madara. (#7832)

---
+++ multisrc/overrides/madara/limascans/src/LimaScans.kt
@@ -0,0 +1,37 @@
+package eu.kanade.tachiyomi.extension.pt.limascans
+
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SManga
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+@Nsfw
+class LimaScans : Madara(
+    "Lima Scans",
+    "http://limascans.xyz/v2",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR"))
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(RateLimitInterceptor(1, 1, TimeUnit.SECONDS))
+        .build()
+
+    override val altName: String = "Nome alternativo: "
+
+    override fun popularMangaSelector() = "div.page-item-detail.manga"
+
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        return GET(baseUrl + manga.url.removePrefix("/v2"), headers)
+    }
+
+    override fun chapterListRequest(manga: SManga): Request {
+        return GET(baseUrl + manga.url.removePrefix("/v2"), headers)
+    }
+}
