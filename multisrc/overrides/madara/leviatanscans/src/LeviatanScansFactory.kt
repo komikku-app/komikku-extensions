28b165f6dfc3ece664ea6271d452bcf79f27a817
Myst <1592048+LeMyst@users.noreply.github.com>
2021-06-25 21:21:11 +0200

    Add support for mangas with seasons (#7878)
    Use chapter name as chapter number

--- multisrc/overrides/madara/leviatanscans/src/LeviatanScansFactory.kt
+++ multisrc/overrides/madara/leviatanscans/src/LeviatanScansFactory.kt
@@ -3,6 +3,8 @@ package eu.kanade.tachiyomi.extension.all.leviatanscans
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
+import eu.kanade.tachiyomi.source.model.SChapter
+import okhttp3.Response
 
 class LeviatanScansFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
@@ -10,5 +12,7 @@ class LeviatanScansFactory : SourceFactory {
         LeviatanScansES(),
     )
 }
-class LeviatanScansEN : Madara("Leviatan Scans", "https://leviatanscans.com", "en")
+class LeviatanScansEN : Madara("Leviatan Scans", "https://leviatanscans.com", "en") {
+    override fun chapterListParse(response: Response): List<SChapter> = super.chapterListParse(response).sortedBy { it.name.toInt() }.reversed()
+}
 class LeviatanScansES : Madara("Leviatan Scans", "https://es.leviatanscans.com", "es")
