198ab904e85317f56f14be64830b95fbdc8650b4
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-06-28 20:09:38 +0800

    add three new sources to madara (#7905)
    * Create MangaDNA.kt
    
    * Update MangaDNA.kt
    
    * add icon
    
    * Create XManga.kt
    
    * add three new sources to madara
    
    * Update MangaDNA.kt

---
+++ multisrc/overrides/madara/xmanga/src/XManga.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.xmanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class XManga : Madara("XManga", "https://xmanga.io", "en")
