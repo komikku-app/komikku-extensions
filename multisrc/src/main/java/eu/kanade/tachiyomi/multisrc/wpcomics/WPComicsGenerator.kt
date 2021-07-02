4caedfcf68ee69cb28adb4d769047791ca459d88
aSimpleGuy <69104545+aSimpleGuy@users.noreply.github.com>
2021-07-02 17:45:09 +0530

    NhatTruyen and NetTruyen url update (#7927)
    * Update NetTruyen.kt
    
    * Update NhatTruyen.kt
    
    * Update WPComicsGenerator.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
@@ -14,8 +14,8 @@ class WPComicsGenerator : ThemeSourceGenerator {
 
     override val sources = listOf(
             MultiLang("MangaToro", "https://mangatoro.com", listOf("en", "ja")),
-            SingleLang("NetTruyen", "https://www.nettruyen.com", "vi", overrideVersionCode = 1),
-            SingleLang("NhatTruyen", "http://nhattruyen.com", "vi", overrideVersionCode = 1),
+            SingleLang("NetTruyen", "http://www.nettruyentop.com", "vi", overrideVersionCode = 2),
+            SingleLang("NhatTruyen", "http://nhattruyentranh.com", "vi", overrideVersionCode = 2),
             SingleLang("TruyenChon", "http://truyenchon.com", "vi", overrideVersionCode = 1),
             SingleLang("XOXO Comics", "https://xoxocomics.com", "en", className = "XoxoComics", overrideVersionCode = 1),
     )
