654436f80dd9faa7f26a8e5d81bd54ecee784118
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-05 20:53:31 +0800

    add few more sources to madara (#7974)
    * icons
    
    * Create ReadAdult.kt
    
    * Create hManhwa.kt
    
    * Update MadaraGenerator.kt
    
    * add todaymic

---
+++ multisrc/overrides/madara/hmanhwa/src/hManhwa.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.hmanhwa
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class hManhwa : Madara("hManhwa", "https://hmanhwa.com", "en")
---
+++ multisrc/overrides/madara/readadult/src/ReadAdult.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.readadult
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class ReadAdult : Madara("Read Adult", "https://readadult.net", "en")
