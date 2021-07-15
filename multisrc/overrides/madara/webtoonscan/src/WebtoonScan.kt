500e6dbf33764c49f68abadc91570564b9461916
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-15 18:15:32 +0800

    added webtoonscan & remove nartag to madara (#8121)
    * icon
    
    * nsfw override
    
    * Delete multisrc/overrides/madara/traduccionesamistosas directory
    
    * add webtoonscan & remove nartag

---
+++ multisrc/overrides/madara/webtoonscan/src/WebtoonScan.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.webtoonscan
+
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+@Nsfw
+class WebtoonScan : Madara("WebtoonScan", "https://webtoonscan.com", "en")
