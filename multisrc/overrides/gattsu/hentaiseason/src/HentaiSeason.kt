d45892ced1aa18dbc59bad00755653c898fc5750
Alessandro Jean <alessandrojean@gmail.com>
2021-07-23 16:40:29 -0300

    Add Gattsu theme to multisrc. (#8245)

---
+++ multisrc/overrides/gattsu/hentaiseason/src/HentaiSeason.kt
@@ -0,0 +1,19 @@
+package eu.kanade.tachiyomi.extension.pt.hentaiseason
+
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.gattsu.Gattsu
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+@Nsfw
+class HentaiSeason : Gattsu(
+    "Hentai Season",
+    "https://hentaiseason.com",
+    "pt-BR"
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(RateLimitInterceptor(1, 2, TimeUnit.SECONDS))
+        .build()
+}
---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/gattsu/GattsuGenerator.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.multisrc.gattsu
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class GattsuGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "gattsu"
+
+    override val themeClass = "Gattsu"
+
+    override val baseVersionCode: Int = 1
+
+    override val sources = listOf(
+    	SingleLang("Hentai Kai", "https://hentaikai.com", "pt-BR", isNsfw = true),
+    	SingleLang("Hentai Season", "https://hentaiseason.com", "pt-BR", isNsfw = true)
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            GattsuGenerator().createAll()
+        }
+    }
+}
+
