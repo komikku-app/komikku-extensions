f13583fc56dfb6791c4c73b8e4cc7681678ec902
zeredouni <80139822+zeredouni@users.noreply.github.com>
2021-07-27 12:10:31 +0000

    Update MadaraGenerator.kt (#8287)

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -85,6 +85,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("Furio Scans", "https://furioscans.com", "pt-BR", overrideVersionCode = 3),
         SingleLang("Fênix Scanlator", "https://fenixscanlator.xyz", "pt-BR", pkgName = "fenixscanlator", className = "FenixScanlator", overrideVersionCode = 2),
         SingleLang("GalaxyDegenScans", "https://gdegenscans.xyz/", "en", overrideVersionCode = 1),
+        SingleLang("Gatemanga", "https://gatemanga.com", "ar"),
         SingleLang("Geass Hentai", "https://geasshentai.xyz", "pt-BR", isNsfw = true, overrideVersionCode = 1),
         SingleLang("Glory Scans", "https://gloryscan.com", "pt-BR", isNsfw = true, overrideVersionCode = 2),
         SingleLang("Gogoscans", "https://www.gogoscans.com", "en"),
