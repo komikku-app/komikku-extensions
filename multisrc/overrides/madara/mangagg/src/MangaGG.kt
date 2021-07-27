02393522b54ec41335247726d9d6a098ef289240
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-27 20:12:07 +0800

    fix two madara sources (#8294)
    * Update MangaGG.kt
    
    * Update WebToon18.kt
    
    * Delete MangaLibrary.kt
    
    * Delete multisrc/overrides/madara/mangalibrary directory
    
    * Update MadaraGenerator.kt

--- multisrc/overrides/madara/mangagg/src/MangaGG.kt
+++ multisrc/overrides/madara/mangagg/src/MangaGG.kt
@@ -1,11 +1,5 @@
 package eu.kanade.tachiyomi.extension.en.mangagg
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.GET
-import okhttp3.Request
 
-class MangaGG : Madara("MangaGG", "https://mangagg.com", "en") {
-
-    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/comic-list/$page?m_orderby=views", headers)
-    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/comic-list/$page?m_orderby=latest", headers)
-}
+class MangaGG : Madara("MangaGG", "https://mangagg.com", "en")
