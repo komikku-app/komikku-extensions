ee1e7a60e7128dd274479225f308550430d311bd
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-07-27 20:11:34 +0800

    add new sources to madara (#8291)
    * icons
    
    * Create Jaiminisboxnet.kt
    
    * Create EternalScans.kt
    
    * Create MangaDistrict.kt
    
    * Create Manganatoinfo.kt
    
    * Create ManhuaZone.kt
    
    * replace mangaweebs icon
    
    * Update MadaraGenerator.kt
    
    * add className
    
    * Update Jaiminisboxnet.kt
    
    * Create Jaiminisboxnet.kt
    
    * Update MadaraGenerator.kt

---
+++ multisrc/overrides/madara/mangadistrict/src/MangaDistrict.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.en.mangadistrict
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Nsfw
+class MangaDistrict : Madara(
+    "Manga District",
+    "https://mangadistrict.com",
+    "en",
+    dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.US)
+) {
+
+}
---
+++ multisrc/overrides/madara/manhuazone/src/ManhuaZone.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.extension.en.manhuazone
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class ManhuaZone : Madara(
+    "ManhuaZone",
+    "https://manhuazone.com",
+    "en",
+    dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.US)
+) {
+
+}
