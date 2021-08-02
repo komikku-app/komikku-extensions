dd4d15020745f976153878aeced1718076530756
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-08-02 22:56:41 +0800

    dragontea: add icon & fix chapter timestamp, mangagg: update icon (#8365)
    * icons
    
    * Create DragonTea.kt
    
    * Update MadaraGenerator.kt
    
    * add missing /

---
+++ multisrc/overrides/madara/dragontea/src/DragonTea.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.extension.en.dragontea
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class DragonTea : Madara(
+    "DragonTea",
+    "https://dragontea.ink/",
+    "en",
+    dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.US)
+) {
+
+}
