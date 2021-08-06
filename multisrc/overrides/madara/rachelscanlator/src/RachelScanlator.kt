1fda9eb35e6b302858cd95c946c515f539f809b5
Alessandro Jean <alessandrojean@gmail.com>
2021-08-06 07:10:44 -0300

    Add two new sources to Madara. (#8414)

---
+++ multisrc/overrides/madara/rachelscanlator/src/RachelScanlator.kt
@@ -0,0 +1,26 @@
+package eu.kanade.tachiyomi.extension.pt.rachelscanlator
+
+import eu.kanade.tachiyomi.annotations.Nsfw
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+@Nsfw
+class RachelScanlator : Madara(
+    "Rachel Scanlator",
+    "https://rachelscanlator.tk",
+    "pt-BR",
+    SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR"))
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(RateLimitInterceptor(1, 2, TimeUnit.SECONDS))
+        .build()
+
+    override val altName: String = "Nome alternativo: "
+
+    override fun popularMangaSelector() = "div.page-item-detail.manga"
+}
