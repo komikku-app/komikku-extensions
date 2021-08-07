1b9ccf1b0f705d1bed2e041633e1f36b154b98f4
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-08-08 00:59:51 +0800

    skymanga: add nsfw tag (#8432)
    * nsfw override
    
    * Update MadaraGenerator.kt
    
    * Update MadaraGenerator.kt
    
    * Update MadaraGenerator.kt

---
+++ multisrc/overrides/madara/skymanga/src/Skymanga.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.skymanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class Skymanga : Madara("Skymanga", "https://skymanga.co", "en")
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -327,7 +327,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("SiXiang Scans", "http://www.sixiangscans.com", "en", overrideVersionCode = 1),
         SingleLang("Sinensis", "https://sinensisscan.com", "pt-BR", overrideVersionCode = 1),
         SingleLang("Siyahmelek", "https://siyahmelek.net", "tr", isNsfw = true, overrideVersionCode = 2),
-        SingleLang("Skymanga", "https://skymanga.co", "en", overrideVersionCode = 1),
+        SingleLang("Skymanga", "https://skymanga.co", "en", isNsfw = true, overrideVersionCode = 2),
         SingleLang("SkyRaws", "https://skyraws.com", "en", overrideVersionCode = 1),
         SingleLang("Sleeping Knight Scans", "https://skscans.com", "en", overrideVersionCode = 2),
         SingleLang("Sleepy Translations", "https://sleepytranslations.com/", "en", overrideVersionCode = 1),
