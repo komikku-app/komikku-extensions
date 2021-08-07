e47132db4cc0e357412e145a0e181f3f54083186
zeredouni <80139822+zeredouni@users.noreply.github.com>
2021-08-07 16:00:34 +0000

    Update MadaraGenerator.kt (#8447)

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -34,7 +34,6 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("Anisa Manga", "https://anisamanga.com", "tr"),
         SingleLang("Anitation Arts", "https://anitationarts.org", "en"),
         SingleLang("ApollComics", "https://apollcomics.xyz", "es", overrideVersionCode = 1),
-        SingleLang("ArabMkr", "https://arabmkr.me", "ar"),
         SingleLang("ArazNovel", "https://www.araznovel.com", "tr", overrideVersionCode = 1),
         SingleLang("Argos Scan", "https://argosscan.com", "pt-BR", overrideVersionCode = 3),
         SingleLang("Arthur Scan", "https://arthurscan.xyz", "pt-BR", overrideVersionCode = 2),
