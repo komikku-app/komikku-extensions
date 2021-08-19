933e566fbad0537fc08acc493183d77582f96c5d
CVIUS <84634607+CVIUS@users.noreply.github.com>
2021-08-19 18:53:56 +0800

    madara: add lumine scans, change IS & SBS icon (#8649)
    * icons
    
    * Create SunshineButterflyScans.kt
    
    * Update MadaraGenerator.kt

---
+++ multisrc/overrides/madara/sunshinebutterflyscans/src/SunshineButterflyScans.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.sunshinebutterflyscans
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.annotations.Nsfw
+
+@Nsfw
+class SunshineButterflyScans : Madara("Sunshine Butterfly Scans", "https://sunshinebutterflyscan.com", "en")
