0a9a074d9ee60af5bf8c11a6cbad4f2731e5ee77
scb261 <65343233+scb261@users.noreply.github.com>
2021-09-14 18:06:22 +0300

    ManhuaES: Filter out ghost chapters (#9055)
    * ManhuaES: Filter out ghost chapters
    
    * Refactor

--- multisrc/overrides/madara/manhuaes/src/ManhuaES.kt
+++ multisrc/overrides/madara/manhuaes/src/ManhuaES.kt
@@ -1,7 +1,32 @@
 package eu.kanade.tachiyomi.extension.en.manhuaes
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Response
 
 class ManhuaES : Madara("Manhua ES", "https://manhuaes.com", "en") {
     override val pageListParseSelector = ".reading-content div.text-left :has(>img)"
+
+    override fun chapterListParse(response: Response): List<SChapter> {
+        var chapterList = super.chapterListParse(response)
+
+        // the site adds a ghost chapter for some entries
+        chapterList.firstOrNull()?.let {
+            if (!isReleasedChapter(it)) {
+                chapterList = chapterList.subList(1, chapterList.size)
+            }
+        }
+
+        return chapterList
+    }
+
+    private fun isReleasedChapter(chapter: SChapter): Boolean {
+        val document = client.newCall(
+            GET(chapter.url, headersBuilder().build())
+        ).execute().asJsoup()
+
+        return document.select(pageListParseSelector).isNotEmpty()
+    }
 }
