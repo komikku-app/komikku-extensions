7cc865e8fb153406be5db77bd61e91e0738eb20f
Alessandro Jean <alessandrojean@gmail.com>
2021-09-25 07:19:40 -0300

    Add a new source to Madara (#9217)
    * Fix missing chapters in FS.
    
    * Add a new source to Madara.

--- multisrc/overrides/madara/momonohanascan/src/MomoNoHanaScan.kt
+++ multisrc/overrides/madara/momonohanascan/src/MomoNoHanaScan.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.pt.furioscans
+package eu.kanade.tachiyomi.extension.pt.momonohanascan
 
 import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
 import eu.kanade.tachiyomi.multisrc.madara.Madara
@@ -7,9 +7,9 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
-class FurioScans : Madara(
-    "Furio Scans",
-    "https://furioscans.com",
+class MomoNoHanaScan : Madara(
+    "Momo no Hana Scan",
+    "https://momonohanascan.com",
     "pt-BR",
     SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR"))
 ) {
