ffd6f7e52acd863d5bd11f3695a68130a457004e
Shiranui <32921531+uknfire@users.noreply.github.com>
2021-10-06 18:06:48 +0800

    Zerobyw: Update baseUrl (#9375)
    * Zerobyw: Change url
    
    * Zerobyw: Update version number

--- src/zh/zerobyw/src/eu/kanade/tachiyomi/extension/zh/zerobyw/Zerobyw.kt
+++ src/zh/zerobyw/src/eu/kanade/tachiyomi/extension/zh/zerobyw/Zerobyw.kt
@@ -17,7 +17,7 @@ class Zerobyw : ParsedHttpSource() {
     override val name: String = "zero搬运网"
     override val lang: String = "zh"
     override val supportsLatest: Boolean = false
-    override val baseUrl: String = "https://www.zerobywps5.com"
+    override val baseUrl: String = "https://www.zerobyw3ds.com"
 
     // Popular
     // Website does not provide popular manga, this is actually latest manga
