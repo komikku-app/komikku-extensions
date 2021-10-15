d3b64152a5fdd7b691d3825f269a7a6a34ed0d4e
Promchai Chooseang <promchai2sin@gmail.com>
2021-10-15 23:26:57 +0700

    Add MikuDoujin and Rh2PlusManga extension (#9469)
    * Add MikuDoujin
    * Add MikuDoujin V.1.2.1
    
    * Update MikuDoujin.kt
    
    * Update MikuDoujin
    * Add ability to search using genre
    
    * Add Rh2PlusManga
    * Add Rh2PlusManga V.1.12.2
    * Use Madara multi-source themes
    
    * Change MikuDoujin gradle and Add Rh2PlusManga generator.
    * update file to suggestion.
    * add Rh2PlusManga to MadaraGenerator.kt.

---
+++ multisrc/overrides/madara/rh2plusmanga/src/Rh2PlusManga.kt
@@ -0,0 +1,61 @@
+package eu.kanade.tachiyomi.extension.th.rh2plusmanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Rh2PlusManga : Madara("Rh2PlusManga", "https://www.rh2plusmanga.com", "th", SimpleDateFormat("d MMMM d yyyy", Locale("th"))) {
+    override val useNewChapterEndpoint = true
+    override fun getGenreList() = listOf(
+        Genre("เหนือธรรมชาติ", "supernatural"),
+        Genre("ทำอาหาร", "cooking"),
+        Genre("สยองขวัญ", "horror"),
+        Genre("ยูริ", "yuri"),
+        Genre("จิตวิทยา", "psychological"),
+        Genre("วัยรุ่น", "seinen"),
+        Genre("ชีวิตประจำวัน", "slice-of-life"),
+        Genre("เค-เว็บตูน", "เค-เว็บตูน"),
+        Genre("ต่างโลก", "ต่างโลก"),
+        Genre("แฟนตาซี", "fantasy"),
+        Genre("ไซ-ไฟ", "sci-fi"),
+        Genre("คอมเมดี้", "comedy"),
+        Genre("โรแมนติก", "romance"),
+        Genre("สำหรับผู้ใหญ่", "adult"),
+        Genre("ยาโอย", "yaoi"),
+        Genre("ศิลปะการต่อสู้", "martial-arts"),
+        Genre("โชเน็น", "shounen"),
+        Genre("ดราม่า", "drama"),
+        Genre("เกิดใหม่", "เกิดใหม่"),
+        Genre("ปริศนา", "mystery"),
+        Genre("ประวัติศาสตร์", "historical"),
+        Genre("มันฮวา", "มันฮวา"),
+        Genre("ผจญภัย", "adventure"),
+        Genre("กีฬา", "sports"),
+        Genre("มังงะ", "manga"),
+        Genre("One shot", "one-shot"),
+        Genre("โชโจ", "shoujo"),
+        Genre("หุ่นยนต์", "mecha"),
+        Genre("แอคชั่น", "action"),
+        Genre("ชีวิตในโรงเรียน", "school-life"),
+        Genre("ฮาเร็ม", "harem"),
+        Genre("ลามก", "ecchi")
+    )
+
+    override val pageListParseSelector = "div.reading-content p code img"
+
+    override fun pageListParse(document: Document): List<Page> {
+        countViews(document)
+
+        return document.select(pageListParseSelector).mapIndexed { index, element ->
+            Page(
+                index,
+                document.location(),
+                element.let {
+                    it.absUrl(if (it.hasAttr("data-src")) "data-src" else "src")
+                }
+            )
+        }
+    }
+}
