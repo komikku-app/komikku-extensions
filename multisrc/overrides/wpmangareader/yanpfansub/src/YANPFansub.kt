b73a6fbedb93b9aefa0e9f57eb3772507a4b1f3a
Alessandro Jean <alessandrojean@gmail.com>
2021-10-20 08:17:30 -0300

    Add a new source to WPMangaReader (closes #9478). (#9552)

---
+++ multisrc/overrides/wpmangareader/yanpfansub/src/YANPFansub.kt
@@ -0,0 +1,22 @@
+package eu.kanade.tachiyomi.extension.pt.yanpfansub
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class YANPFansub : WPMangaReader(
+    "YANP Fansub",
+    "https://yanpfansub.com",
+    "pt-BR",
+    dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("pt", "BR"))
+) {
+
+    override val client: OkHttpClient = network.cloudflareClient.newBuilder()
+        .addNetworkInterceptor(RateLimitInterceptor(1, 2, TimeUnit.SECONDS))
+        .build()
+
+    override val altName = "Nome alternativo: "
+}
