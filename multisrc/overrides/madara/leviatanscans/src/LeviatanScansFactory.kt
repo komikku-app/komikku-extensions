8e34cc2a8ebd545345269e7d1040e2f88ee81b99
Basara-Hatake <53382992+Basara-Hatake@users.noreply.github.com>
2021-11-02 15:51:58 +0100

    leviatanscans replace random url part (#9717)

--- multisrc/overrides/madara/leviatanscans/src/LeviatanScansFactory.kt
+++ multisrc/overrides/madara/leviatanscans/src/LeviatanScansFactory.kt
@@ -3,6 +3,9 @@ package eu.kanade.tachiyomi.extension.all.leviatanscans
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.source.Source
 import eu.kanade.tachiyomi.source.SourceFactory
+import eu.kanade.tachiyomi.source.model.SChapter
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Element
 
 class LeviatanScansFactory : SourceFactory {
     override fun createSources(): List<Source> = listOf(
@@ -12,7 +15,43 @@ class LeviatanScansFactory : SourceFactory {
 }
 class LeviatanScansEN : Madara("Leviatan Scans", "https://leviatanscans.com", "en") {
     override val useNewChapterEndpoint: Boolean = true
+
+    override fun popularMangaFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.popularMangaFromElement(element))
+
+    override fun latestUpdatesFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.latestUpdatesFromElement(element))
+
+    override fun searchMangaFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.searchMangaFromElement(element))
+
+    override fun chapterFromElement(element: Element) =
+        replaceRandomUrlPartInChapter(super.chapterFromElement(element))
 }
 class LeviatanScansES : Madara("Leviatan Scans", "https://es.leviatanscans.com", "es") {
     override val useNewChapterEndpoint: Boolean = true
+
+    override fun popularMangaFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.popularMangaFromElement(element))
+
+    override fun latestUpdatesFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.latestUpdatesFromElement(element))
+
+    override fun searchMangaFromElement(element: Element) =
+        replaceRandomUrlPartInManga(super.searchMangaFromElement(element))
+
+    override fun chapterFromElement(element: Element) =
+        replaceRandomUrlPartInChapter(super.chapterFromElement(element))
+}
+
+fun Madara.replaceRandomUrlPartInManga(manga: SManga): SManga {
+    val split = manga.url.split("/")
+    manga.url = split.slice(split.indexOf("manga") until split.size).joinToString("/", "/")
+    return manga
+}
+
+fun Madara.replaceRandomUrlPartInChapter(chapter: SChapter): SChapter {
+    val split = chapter.url.split("/")
+    chapter.url = baseUrl + split.slice(split.indexOf("manga") until split.size).joinToString("/", "/")
+    return chapter
 }
