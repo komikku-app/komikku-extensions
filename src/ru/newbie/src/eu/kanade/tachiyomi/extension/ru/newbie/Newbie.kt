bf9a02cf3f4210bceb5f98f8e4e5c75918324250
e-shl <35057681+e-shl@users.noreply.github.com>
2021-11-14 16:10:16 +0500

    [RU]Newbie new domain (#9808)

--- src/ru/newbie/src/eu/kanade/tachiyomi/extension/ru/newbie/Newbie.kt
+++ src/ru/newbie/src/eu/kanade/tachiyomi/extension/ru/newbie/Newbie.kt
@@ -38,7 +38,7 @@ import java.util.Locale
 class Newbie : HttpSource() {
     override val name = "Newbie"
 
-    override val baseUrl = "https://newbie-tl.ru"
+    override val baseUrl = "https://newmanga.org"
 
     override val lang = "ru"
 
@@ -353,8 +353,8 @@ class Newbie : HttpSource() {
         CheckFilter("яой", "32"),
     )
     companion object {
-        private const val API_URL = "https://api.newbie-tl.ru/v2"
-        private const val IMAGE_URL = "https://storage.newbie-tl.ru"
+        private const val API_URL = "https://api.newmanga.org/v2"
+        private const val IMAGE_URL = "https://storage.newmanga.org"
     }
     private val json: Json by injectLazy()
 }
