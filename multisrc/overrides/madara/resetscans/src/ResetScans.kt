79974bc635ef97c5cfed9f07b01196dde7055042
Myst <1592048+LeMyst@users.noreply.github.com>
2021-11-28 17:14:52 +0100

    Update Reset Scans (#9944)
    * Add default DateFormat to dd/MM/yyyy
    * Override date_upload of function chapterFromElement()

--- multisrc/overrides/madara/resetscans/src/ResetScans.kt
+++ multisrc/overrides/madara/resetscans/src/ResetScans.kt
@@ -1,7 +1,20 @@
 package eu.kanade.tachiyomi.extension.en.resetscans
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SChapter
+import org.jsoup.nodes.Element
+import java.util.Locale
 
-class ResetScans : Madara("Reset Scans", "https://reset-scans.com", "en") {
+class ResetScans : Madara("Reset Scans", "https://reset-scans.com", "en", java.text.SimpleDateFormat("dd/MM/yyyy", Locale.US)) {
     override val useNewChapterEndpoint = true
+
+    override fun chapterFromElement(element: Element): SChapter {
+        val chapter = super.chapterFromElement(element)
+
+        with(element) {
+            chapter.date_upload = select("span.chapter-release-date i").firstOrNull()?.text().let { parseChapterDate(it) }
+        }
+
+        return chapter
+    }
 }
