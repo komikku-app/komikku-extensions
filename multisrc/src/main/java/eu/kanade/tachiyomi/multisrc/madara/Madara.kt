15f7d1e616004bc76207005a995a4cc716b6d106
Quang Kieu <quangkieu@users.noreply.github.com>
2021-11-29 06:12:30 -0500

    Fix #8835 as missing CDN Referer. Might fix other issues of Referer (#9957)
    Co-authored-by: quangkieu <quangkieu1993@gmail.com>

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
@@ -54,6 +54,7 @@ abstract class Madara(
 
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
         .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/78.0$userAgentRandomizer")
+        .add("Referer", baseUrl)
 
     // Popular Manga
 
