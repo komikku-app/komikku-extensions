12dd0a19d00bdbb5bd88f09bc9269adf75440d6f
KoSChicken <hd9421@outlook.com>
2021-12-24 20:59:57 +0800

    fix jinmantiantang (#10211)

--- src/zh/jinmantiantang/src/eu/kanade/tachiyomi/extension/zh/jinmantiantang/Jinmantiantang.kt
+++ src/zh/jinmantiantang/src/eu/kanade/tachiyomi/extension/zh/jinmantiantang/Jinmantiantang.kt
@@ -142,7 +142,7 @@ class Jinmantiantang : ConfigurableSource, ParsedHttpSource() {
 
     override fun popularMangaNextPageSelector(): String = "a.prevnext"
     override fun popularMangaSelector(): String {
-        val baseSelector = ".list-col .well-sm"
+        val baseSelector = "div.col-xs-6.col-sm-6.col-md-4.col-lg-3.list-col"
         val removedGenres = preferences.getString("BLOCK_GENRES_LIST", "")!!.substringBefore("//").trim()
         // Extra selector is jquery-like selector, it uses regex to match element.text().
         // If string after 標籤 contains any word of removedGenres, the element would be ignored.
