28e42192649f1b76d324f0cd733a65d45d8a9653
Troy121 <tose.esnu@gmail.com>
2022-01-09 17:26:45 +0100

    Realm Scans: Fix image parsing (#10385)

--- multisrc/overrides/wpmangareader/realmscans/src/RealmScans.kt
+++ multisrc/overrides/wpmangareader/realmscans/src/RealmScans.kt
@@ -1,5 +1,20 @@
 package eu.kanade.tachiyomi.extension.en.realmscans
 
 import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
+import eu.kanade.tachiyomi.source.model.Page
+import org.jsoup.nodes.Document
 
-class RealmScans : WPMangaReader("Realm Scans", "https://realmscans.com", "en", "/series")
+class RealmScans : WPMangaReader("Realm Scans", "https://realmscans.com", "en", "/series") {
+    override fun pageListParse(document: Document): List<Page> {
+        countViews(document)
+
+        return document.select(pageSelector).mapIndexed { i, img ->
+            val url = if (img.attr("data-wpfc-original-src").isEmpty())
+                img.attr("abs:src")
+            else
+                img.attr("data-wpfc-original-src")
+
+            Page(i, "", url)
+        }
+    }
+}
