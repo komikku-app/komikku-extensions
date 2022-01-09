fa7e1e75d01376525d7d15f1df494cd90dd7cac6
Troy121 <tose.esnu@gmail.com>
2022-01-09 17:25:17 +0100

    KissLove: Update url to new domain (#10389)

--- multisrc/overrides/fmreader/kisslove/src/KissLove.kt
+++ multisrc/overrides/fmreader/kisslove/src/KissLove.kt
@@ -4,6 +4,6 @@ import eu.kanade.tachiyomi.multisrc.fmreader.FMReader
 import eu.kanade.tachiyomi.source.model.Page
 import org.jsoup.nodes.Document
 
-class KissLove : FMReader("KissLove", "https://kissaway.net", "ja") {
+class KissLove : FMReader("KissLove", "https://klmag.net", "ja") {
     override fun pageListParse(document: Document): List<Page> = base64PageListParse(document)
 }
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
@@ -17,7 +17,7 @@ class FMReaderGenerator : ThemeSourceGenerator {
 
     override val sources = listOf(
         SingleLang("Epik Manga", "https://www.epikmanga.com", "tr"),
-        SingleLang("KissLove", "https://kissaway.net", "ja"),
+        SingleLang("KissLove", "https://klmag.net", "ja", overrideVersionCode = 1),
         SingleLang("Manga-TR", "https://manga-tr.com", "tr", className = "MangaTR"),
         SingleLang("Manhwa18", "https://manhwa18.com", "en", isNsfw = true, overrideVersionCode = 1),
         MultiLang("Manhwa18.net", "https://manhwa18.net", listOf("en", "ko"), className = "Manhwa18NetFactory", isNsfw = true),
