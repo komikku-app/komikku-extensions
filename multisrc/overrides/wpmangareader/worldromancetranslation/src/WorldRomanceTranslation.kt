0353dbb02b7762395a6b467aa0a8d116168fb1cb
Riztard Lanthorn <riyanluqman@gmail.com>
2022-01-20 17:01:08 +0700

    WRT: fix 404 issue (#10518)

--- multisrc/overrides/wpmangareader/worldromancetranslation/src/WorldRomanceTranslation.kt
+++ multisrc/overrides/wpmangareader/worldromancetranslation/src/WorldRomanceTranslation.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class WorldRomanceTranslation : WPMangaReader("World Romance Translation", "https://wrt.my.id", "id", "/komik", SimpleDateFormat("MMMM dd, yyyy", Locale("id"))) {
+class WorldRomanceTranslation : WPMangaReader("World Romance Translation", "https://wrt.my.id", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))) {
     override val projectPageString = "/project-wrt"
 
     override val hasProjectPage = true
