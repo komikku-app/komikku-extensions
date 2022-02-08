8b18e2f12e41fa7fb25db24688cca1d22e8398cd
Yan <i@yinyan.fr>
2022-02-08 15:08:44 +0100

    Zerobyw: Update baseUrl (#10750)

--- src/zh/zerobyw/src/eu/kanade/tachiyomi/extension/zh/zerobyw/Zerobyw.kt
+++ src/zh/zerobyw/src/eu/kanade/tachiyomi/extension/zh/zerobyw/Zerobyw.kt
@@ -17,7 +17,7 @@ class Zerobyw : ParsedHttpSource() {
     override val name: String = "zero搬运网"
     override val lang: String = "zh"
     override val supportsLatest: Boolean = false
-    override val baseUrl: String = "https://www.zerobywdlss.com"
+    override val baseUrl: String = "https://www.zerobywrevi.com"
 
     // Popular
     // Website does not provide popular manga, this is actually latest manga
