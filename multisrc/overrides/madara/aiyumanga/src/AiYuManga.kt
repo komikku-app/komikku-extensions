e76f62f8c8710b3ef45feabcf8471bd1d578eb00
Vetle Ledaal <vetle.ledaal@gmail.com>
2022-02-20 11:11:52 +0000

    Add more Madara sites (#10868)
    * Madara: add sites
    
    * icons
    
    * Update multisrc/overrides/madara/gooffansub/src/GoofFansub.kt
    
    Co-authored-by: Alessandro Jean <alessandrojean@gmail.com>
    
    * Update multisrc/overrides/madara/pirulitorosa/src/PirulitoRosa.kt
    
    Co-authored-by: Alessandro Jean <alessandrojean@gmail.com>
    
    Co-authored-by: Alessandro Jean <alessandrojean@gmail.com>

---
+++ multisrc/overrides/madara/aiyumanga/src/AiYuManga.kt
@@ -0,0 +1,14 @@
+package eu.kanade.tachiyomi.extension.es.aiyumanga
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class AiYuManga : Madara(
+    "AiYuManga",
+    "https://aiyumangascanlation.com",
+    "es",
+    SimpleDateFormat("d 'de' MMM 'de' yyy", Locale("es"))
+) {
+    override val useNewChapterEndpoint = true
+}
---
+++ multisrc/overrides/madara/mangacim/src/Mangacim.kt
@@ -0,0 +1,13 @@
+package eu.kanade.tachiyomi.extension.tr.mangacim
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+
+class Mangacim : Madara(
+    "Mangacim",
+    "https://www.mangacim.com",
+    "tr",
+    SimpleDateFormat("MMM d, yyy", Locale("tr"))
+)
