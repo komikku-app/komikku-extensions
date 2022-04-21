d46e7b60c7f9bf71ce0172779e8fc4ca57112ace
Alexander Chernov <alexander@chernov.it>
2022-04-21 20:41:22 +0100

    chore: changed domain for dreammanga (#11557)

--- multisrc/overrides/madara/dreammanga/src/DreamManga.kt
+++ multisrc/overrides/madara/dreammanga/src/DreamManga.kt
@@ -4,4 +4,4 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class DreamManga : Madara("Dream Manga", "https://en.ruyamanga.com", "en", SimpleDateFormat("dd/MM/yyyy", Locale.US))
+class DreamManga : Madara("Dream Manga", "https://www.swarmmanga.com", "en", SimpleDateFormat("dd/MM/yyyy", Locale.US))
