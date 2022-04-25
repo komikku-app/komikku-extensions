7113c5166a86590d8663f725e7b77087ecb29879
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2022-04-25 19:55:36 -0300

    Add a new source to Madara (closes #11593). (#11610)

---
+++ multisrc/overrides/madara/vapohentai/src/VapoHentai.kt
@@ -0,0 +1,20 @@
+package eu.kanade.tachiyomi.extension.pt.vapohentai
+
+import eu.kanade.tachiyomi.lib.ratelimit.RateLimitInterceptor
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class VapoHentai : Madara(
+    "Vapo Hentai",
+    "http://xxx.vaposcans.com",
+    "pt-BR",
+    SimpleDateFormat("dd 'de' MMMMM 'de' yyyy", Locale("pt", "BR"))
+) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .addInterceptor(RateLimitInterceptor(1, 2, TimeUnit.SECONDS))
+        .build()
+}
