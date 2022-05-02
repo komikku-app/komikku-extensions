533eec004d1aa9e9d9c188eb8fe2cc4a77d4ee52
Promchai Chooseang <promchai2sin@gmail.com>
2022-05-03 04:34:35 +0700

    Fix some Thai Madara source (#11703)
    * Clean up code and disable NonMangaItems Filter
    
    * Clean up code and disable NonMangaItems Filter
    
    * Clean up code and Change baseUrl
    
    * Fix Url problem
    
    * Change requests url due to inaccessibility of the admin-ajax.php page
    
    * Clean up code and disable NonMangaItems Filter
    
    Clean up code and disable NonMangaItems Filter
    
    Clean up code and Change baseUrl
    
    Fix Url problem
    
    Change requests url due to inaccessibility of the admin-ajax.php page

--- multisrc/overrides/madara/cattranslator/src/CatTranslator.kt
+++ multisrc/overrides/madara/cattranslator/src/CatTranslator.kt
@@ -1,16 +1,33 @@
 package eu.kanade.tachiyomi.extension.th.cattranslator
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.network.POST
-import okhttp3.CacheControl
-import okhttp3.Request
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Element
 
-class CatTranslator : Madara("CAT-translator", "https://cat-translator.com", "th") {
-    override fun popularMangaRequest(page: Int): Request =
-        POST("$baseUrl/manga/wp-admin/admin-ajax.php", formHeaders, formBuilder(page, true).build(), CacheControl.FORCE_NETWORK)
+class CatTranslator : Madara(
+    "CAT-translator",
+    "https://cats-translator.com/manga",
+    "th"
+) {
+    private fun parseMangaFromElement(element: Element, isSearch: Boolean): SManga {
+        val manga = SManga.create()
 
-    override fun latestUpdatesRequest(page: Int): Request =
-        POST("$baseUrl/manga/wp-admin/admin-ajax.php", formHeaders, formBuilder(page, false).build(), CacheControl.FORCE_NETWORK)
+        with(element) {
+            select(if (isSearch) "div.post-title a" else popularMangaUrlSelector).first()?.let {
+                manga.setUrlWithoutDomain(it.attr("abs:href"))
+                manga.url = manga.url.removePrefix("/manga")
+                manga.title = it.ownText()
+            }
 
-    override fun searchPage(page: Int): String = "manga/page/$page/"
+            select("img").first()?.let {
+                manga.thumbnail_url = imageFromElement(it)
+            }
+        }
+
+        return manga
+    }
+
+    override fun popularMangaFromElement(element: Element) = parseMangaFromElement(element, false)
+
+    override fun searchMangaFromElement(element: Element) = parseMangaFromElement(element, true)
 }
--- multisrc/overrides/madara/rh2plusmanga/src/Rh2PlusManga.kt
+++ multisrc/overrides/madara/rh2plusmanga/src/Rh2PlusManga.kt
@@ -1,27 +1,9 @@
 package eu.kanade.tachiyomi.extension.th.rh2plusmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.Page
-import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class Rh2PlusManga : Madara("Rh2PlusManga", "https://www.rh2plusmanga.com", "th", SimpleDateFormat("d MMMM yyyy", Locale("th"))) {
-    override val useNewChapterEndpoint = true
-
-    override val pageListParseSelector = "div.reading-content p code img"
-
-    override fun pageListParse(document: Document): List<Page> {
-        countViews(document)
-
-        return document.select(pageListParseSelector).mapIndexed { index, element ->
-            Page(
-                index,
-                document.location(),
-                element.let {
-                    it.absUrl(if (it.hasAttr("data-src")) "data-src" else "src")
-                }
-            )
-        }
-    }
+    override val filterNonMangaItems = false
 }
