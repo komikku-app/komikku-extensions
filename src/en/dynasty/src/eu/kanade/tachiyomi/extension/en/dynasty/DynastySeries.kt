723bce9a5a1c64ad1e4315197c0e2257a91d8bfd
lord-ne <34069872+lord-ne@users.noreply.github.com>
2022-05-26 06:29:11 -0400

    Also search series from chapter links (#11970)

--- src/en/dynasty/src/eu/kanade/tachiyomi/extension/en/dynasty/DynastySeries.kt
+++ src/en/dynasty/src/eu/kanade/tachiyomi/extension/en/dynasty/DynastySeries.kt
@@ -2,9 +2,11 @@ package eu.kanade.tachiyomi.extension.en.dynasty
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
+import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Request
 import org.jsoup.nodes.Document
+import rx.Observable
 
 class DynastySeries : DynastyScans() {
 
@@ -16,6 +18,16 @@ class DynastySeries : DynastyScans() {
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         return GET("$baseUrl/search?q=$query&classes%5B%5D=Series&sort=&page=$page", headers)
+    }
+
+    override fun fetchSearchManga(page: Int, query: String, filters: FilterList): Observable<MangasPage> {
+        if (query.startsWith("manga:chapters:")) {
+            val seriesName = Regex("""manga:chapters:(.*?)_ch\d+""").matchEntire(query)?.groups?.get(1)?.value
+            if (seriesName != null) {
+                return super.fetchSearchManga(page, "manga:$searchPrefix:$seriesName", filters)
+            }
+        }
+        return super.fetchSearchManga(page, query, filters)
     }
 
     override fun mangaDetailsParse(document: Document): SManga {
