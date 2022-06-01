b4df51d652335f99a61d8715498d47fe6b3f06bb
Saud-97 <39028181+Saud-97@users.noreply.github.com>
2022-06-01 05:43:28 +0300

    Renamed iiManga -> ARESManga (#12004)
    * Renamed iiManga -> ARESManga
    
    * Changed class name
    
    * formatting

--- multisrc/overrides/wpmangareader/iimanga/src/iiMANGA.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.ar.iimanga
-
-import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class iiMANGA : WPMangaReader("iiMANGA", "https://aresmanga.com", "ar", dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("ar")))
