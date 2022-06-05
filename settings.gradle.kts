d925c303939e6b6d7683cc1a6b26a42f4c7775a0
arkon <arkon@users.noreply.github.com>
2022-06-04 20:55:39 -0400

    Build modules in chunks (#12067)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ project(":lib-ratelimit").projectDir = File("lib/ratelimit")
 include(":lib-dataimage")
 project(":lib-dataimage").projectDir = File("lib/dataimage")
 
-if (System.getenv("CI") == null) {
+if (System.getenv("CI") == null || System.getenv("CI_MODULE_GEN") == "true") {
     // Local development (full project build)
 
     include(":multisrc")
@@ -20,7 +20,7 @@ if (System.getenv("CI") == null) {
             project(name).projectDir = File("src/${dir.name}/${subdir.name}")
         }
     }
-    // Loads generated extensions from multisrc
+    // Loads all generated extensions from multisrc
     File(rootDir, "generated-src").eachDir { dir ->
         dir.eachDir { subdir ->
             val name = ":extensions:multisrc:${dir.name}:${subdir.name}"
@@ -44,36 +44,43 @@ if (System.getenv("CI") == null) {
     // Running in CI (GitHub Actions)
 
     val isMultisrc = System.getenv("CI_MULTISRC") == "true"
-    val lang = System.getenv("CI_MATRIX_LANG")
+    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
+    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
 
     if (isMultisrc) {
         include(":multisrc")
         project(":multisrc").projectDir = File("multisrc")
 
         // Loads generated extensions from multisrc
-        File(rootDir, "generated-src").eachDir { dir ->
-            if (dir.name == lang) {
-                dir.eachDir { subdir ->
-                    val name = ":extensions:multisrc:${dir.name}:${subdir.name}"
-                    include(name)
-                    project(name).projectDir = File("generated-src/${dir.name}/${subdir.name}")
-                }
-            }
+        File(rootDir, "generated-src").getChunk(chunk, chunkSize)?.forEach {
+            val name = ":extensions:multisrc:${it.parentFile.name}:${it.name}"
+            println(name)
+            include(name)
+            project(name).projectDir = File("generated-src/${it.parentFile.name}/${it.name}")
         }
     } else {
-        // Loads all extensions
-        File(rootDir, "src").eachDir { dir ->
-            if (dir.name == lang) {
-                dir.eachDir { subdir ->
-                    val name = ":extensions:individual:${dir.name}:${subdir.name}"
-                    include(name)
-                    project(name).projectDir = File("src/${dir.name}/${subdir.name}")
-                }
-            }
+        // Loads individual extensions
+        File(rootDir, "src").getChunk(chunk, chunkSize)?.forEach {
+            val name = ":extensions:individual:${it.parentFile.name}:${it.name}"
+            println(name)
+            include(name)
+            project(name).projectDir = File("src/${it.parentFile.name}/${it.name}")
         }
     }
 }
 
-inline fun File.eachDir(block: (File) -> Unit) {
+fun File.getChunk(chunk: Int, chunkSize: Int): List<File>? {
+    return listFiles()
+        // Lang folder
+        ?.filter { it.isDirectory }
+        // Extension subfolders
+        ?.mapNotNull { dir -> dir.listFiles()?.filter { it.isDirectory } }
+        ?.flatten()
+        ?.sortedBy { it.name }
+        ?.chunked(chunkSize)
+        ?.get(chunk)
+}
+
+fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
