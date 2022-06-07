45d6042c81258896ac8450a008a286836561a522
Riztard Lanthorn <riyanluqman@gmail.com>
2022-06-07 17:12:31 +0700

    MangaBuddy: increasing rate limit to avoid ban (#12105)
    - increasing rate limit from 5/sec -> 1/sec

--- multisrc/overrides/madtheme/mangabuddy/src/MangaBuddy.kt
+++ multisrc/overrides/madtheme/mangabuddy/src/MangaBuddy.kt
@@ -4,7 +4,6 @@ import eu.kanade.tachiyomi.multisrc.madtheme.MadTheme
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
-import java.util.concurrent.TimeUnit
 
 class MangaBuddy : MadTheme(
     "MangaBuddy",
@@ -12,35 +11,10 @@ class MangaBuddy : MadTheme(
     "en"
 ) {
     override val client: OkHttpClient = network.cloudflareClient.newBuilder()
-        .rateLimitHost(
-            "https://s1.mbcdnv1.xyz".toHttpUrl(),
-            5,
-            1,
-            TimeUnit.SECONDS
-        )
-        .rateLimitHost(
-            "https://s1.mbcdnv2.xyz".toHttpUrl(),
-            1,
-            2,
-            TimeUnit.SECONDS
-        )
-        .rateLimitHost(
-            "https://s1.mbcdnv3.xyz".toHttpUrl(),
-            1,
-            2,
-            TimeUnit.SECONDS
-        )
-        .rateLimitHost(
-            "https://s1.mbcdnv4.xyz".toHttpUrl(),
-            1,
-            2,
-            TimeUnit.SECONDS
-        )
-        .rateLimitHost(
-            "https://s1.mbcdnv5.xyz".toHttpUrl(),
-            1,
-            2,
-            TimeUnit.SECONDS
-        )
+        .rateLimitHost("https://s1.mbcdnv1.xyz".toHttpUrl(), 1, 1)
+        .rateLimitHost("https://s1.mbcdnv2.xyz".toHttpUrl(), 1, 2)
+        .rateLimitHost("https://s1.mbcdnv3.xyz".toHttpUrl(), 1, 2)
+        .rateLimitHost("https://s1.mbcdnv4.xyz".toHttpUrl(), 1, 2)
+        .rateLimitHost("https://s1.mbcdnv5.xyz".toHttpUrl(), 1, 2)
         .build()
 }
