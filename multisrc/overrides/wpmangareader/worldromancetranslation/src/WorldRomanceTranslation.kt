ada3d40bee822b465ef1e5b91e5a47b6c2cdc3de
Masga <45129872+mxsgx@users.noreply.github.com>
2022-07-02 06:06:33 +0700

    World Romance Translation: fix thumbnail (#12384)
    * World Romance Translation: fix cover image
    
    They change image source attribute again.
    
    * refactor
    
    Co-authored-by: Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
    
    Co-authored-by: Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>

--- multisrc/overrides/wpmangareader/worldromancetranslation/src/WorldRomanceTranslation.kt
+++ multisrc/overrides/wpmangareader/worldromancetranslation/src/WorldRomanceTranslation.kt
@@ -1,12 +1,18 @@
 package eu.kanade.tachiyomi.extension.id.worldromancetranslation
 
 import eu.kanade.tachiyomi.multisrc.wpmangareader.WPMangaReader
+import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Headers
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class WorldRomanceTranslation : WPMangaReader("World Romance Translation", "https://wrt.my.id", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))) {
+class WorldRomanceTranslation : WPMangaReader(
+    "World Romance Translation",
+    "https://wrt.my.id",
+    "id",
+    dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id"))
+) {
     override val projectPageString = "/project-wrt"
 
     override val hasProjectPage = true
@@ -15,7 +21,15 @@ class WorldRomanceTranslation : WPMangaReader("World Romance Translation", "http
         return super.headersBuilder().add("Referer", baseUrl)
     }
 
-    override fun mangaDetailsParse(document: Document) = super.mangaDetailsParse(document).apply {
-        thumbnail_url = document.select(seriesThumbnailSelector).attr("abs:data-lazy-src")
+    override fun mangaDetailsParse(document: Document): SManga {
+        val thumbnail = document.select(seriesThumbnailSelector)
+
+        return super.mangaDetailsParse(document).apply {
+            thumbnail_url = when {
+                thumbnail.hasAttr("data-lazy-src") -> thumbnail.attr("abs:data-lazy-src") // lazyload
+                thumbnail.hasAttr("data-src") -> thumbnail.attr("abs:data-src") // with javascript
+                else -> thumbnail.attr("src")
+            }
+        }
     }
 }
