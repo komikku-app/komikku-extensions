5e6acd100cf378e74701fa1ef1f2e4a2d34dfd58
beerpsi <92439990+beerpiss@users.noreply.github.com>
2022-07-24 20:36:59 +0700

    new multisrc theme: MyMangaCMS (#12702)
    * Initial MyMangaCMS commit
    
    * Finish up the new template
    
    * Add URL intent filter
    
    * more lenient rate limit since the website loads like a ton of shit
    
    * Delete scaffold.py

---
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mymangacms/MyMangaCMSGenerator.kt
@@ -0,0 +1,38 @@
+package eu.kanade.tachiyomi.multisrc.mymangacms
+
+import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceGenerator
+
+class MyMangaCMSGenerator : ThemeSourceGenerator {
+
+    override val themePkg = "mymangacms"
+
+    override val themeClass = "MyMangaCMS"
+
+    override val baseVersionCode: Int = 1
+
+    override val sources = listOf(
+        SingleLang(
+            "TruyenTranhLH",
+            "https://truyentranhlh.net",
+            "vi",
+            overrideVersionCode = 9
+        ),
+        SingleLang(
+            "Phê Manga",
+            "https://phemanga.net",
+            "vi",
+            true,
+            "PheManga",
+            "phemanga",
+        ),
+        SingleLang("LKDTT", "https://lkdtt.com", "vi", true)
+    )
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MyMangaCMSGenerator().createAll()
+        }
+    }
+}
