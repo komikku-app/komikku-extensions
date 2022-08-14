6f448ed46ce2c6e12985d4b077b47f9b66d14f27
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-08-14 18:49:42 +0600

    Update Mangasushi url (#12972)
    Also sort sources in MadaraGenerator

--- multisrc/overrides/madara/mangasushi/src/Mangasushi.kt
+++ multisrc/overrides/madara/mangasushi/src/Mangasushi.kt
@@ -2,6 +2,6 @@ package eu.kanade.tachiyomi.extension.en.mangasushi
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class Mangasushi : Madara("Mangasushi", "https://mangasushi.net", "en") {
+class Mangasushi : Madara("Mangasushi", "https://mangasushi.org", "en") {
     override val useNewChapterEndpoint: Boolean = true
 }
