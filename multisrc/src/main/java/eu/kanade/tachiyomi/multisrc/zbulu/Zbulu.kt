66534d18993dfb8b67509df8a993e4ed8c550984
Martin <HafisCZ@users.noreply.github.com>
2022-08-18 03:27:47 +0100

    HolyManga: Updated image src (#13055)
    * HolyManga: use src if data-src does no exist
    
    * Updated base version
    
    * Fixed typo

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/zbulu/Zbulu.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/zbulu/Zbulu.kt
@@ -192,7 +192,7 @@ abstract class Zbulu(
 
     override fun pageListParse(document: Document): List<Page> {
         return document.select("div.chapter-content img").mapIndexed { i, img ->
-            Page(i, "", img.attr("abs:data-src"))
+            Page(i, "", img.attr(if (img.hasAttr("data-src")) "abs:data-src" else "abs:src"))
         }
     }
 
