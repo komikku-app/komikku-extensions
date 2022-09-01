82a5dc8190d864f9da3db00c3e3f9c1a7039d7ed
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2022-09-01 16:14:54 -0400

    Update Madara Sources (#13298)
    Add MangaOneLove
    Update MangaChill

--- multisrc/overrides/madara/mangachill/src/MangaChill.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangachill
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class MangaChill : Madara("Manga Chill", "https://mangachill.me", "en", SimpleDateFormat("dd/MM/yyyy", Locale.US)) {
-    override val useNewChapterEndpoint: Boolean = true
-}
