4b956148e1da2b61205e3f205c84d2a23ab984b5
happywillow0 <45346080+happywillow0@users.noreply.github.com>
2022-09-23 17:44:21 -0400

    MangaThemesia Updates  (#13553)
    * Migrate Walpurgi Scan
    
    * Update overrideVersionCode
    
    Update version code to account for Madara version
    
    * Move Uzay Manga
    
    * Move MangaWT
    
    * Add MangaWT override
    
    * Add Clayrer

--- multisrc/overrides/madara/walpurgisscan/src/WalpurgisScan.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.it.walpurgisscan
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class WalpurgisScan : Madara("Walpurgi Scan", "https://walpurgiscan.altervista.org", "it", SimpleDateFormat("dd MMMMM yyyy", Locale("it"))) {
-    override val id = 6566957355096372149
-}
---
+++ multisrc/overrides/mangathemesia/clayrer/src/Clayrer.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.es.clayrer
+
+import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Clayrer : MangaThemesia("Clayrer", "https://clayrer.net", "es", dateFormat = SimpleDateFormat("MMM d, yyyy", Locale("es")))
