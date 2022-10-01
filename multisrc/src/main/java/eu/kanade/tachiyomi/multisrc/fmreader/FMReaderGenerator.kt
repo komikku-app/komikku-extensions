16d4610115bcb4a712a07b14b5e842066a6d2c26
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-10-01 18:03:45 +0600

    KSGroupScans: Don't assign date_upload to chapters (#13669)
    * KSGroupScans: Don't assign uploadDate to chapters
    
    Date related stuff is highly broken in the site. Chapters from 2~3 months ago appear as 1 years ago
    
    * Cleanup FMReaderGenerator
    
    * Fix import

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/fmreader/FMReaderGenerator.kt
@@ -12,22 +12,16 @@ class FMReaderGenerator : ThemeSourceGenerator {
 
     override val baseVersionCode: Int = 7
 
-    /** For future sources: when testing and popularMangaRequest() returns a Jsoup error instead of results
-     *  most likely the fix is to override popularMangaNextPageSelector()   */
-
     override val sources = listOf(
+        MultiLang("Manhwa18.net", "https://manhwa18.net", listOf("en", "ko"), className = "Manhwa18NetFactory", isNsfw = true),
         SingleLang("Epik Manga", "https://www.epikmanga.com", "tr"),
         SingleLang("KissLove", "https://klmanga.com", "ja", overrideVersionCode = 2),
+        SingleLang("KSGroupScans", "https://ksgroupscans.com", "en", overrideVersionCode = 2),
         SingleLang("Manga-TR", "https://manga-tr.com", "tr", className = "MangaTR", overrideVersionCode = 1),
-        SingleLang("Manhwa18", "https://manhwa18.com", "en", isNsfw = true, overrideVersionCode = 2),
-        MultiLang("Manhwa18.net", "https://manhwa18.net", listOf("en", "ko"), className = "Manhwa18NetFactory", isNsfw = true),
-        SingleLang("WeLoveManga", "https://weloma.art", "ja", pkgName = "rawlh", overrideVersionCode = 4),
-        SingleLang("Say Truyen", "https://saytruyen.net", "vi", overrideVersionCode = 2),
         SingleLang("ManhuaRock", "https://manhuarock.net", "vi", overrideVersionCode = 1),
-        SingleLang("KSGroupScans", "https://ksgroupscans.com", "en", overrideVersionCode = 1),
-        // Sites that went down
-        // SingleLang("18LHPlus", "https://18lhplus.com", "en", className = "EighteenLHPlus"),
-        // SingleLang("HanaScan (RawQQ)", "https://hanascan.com", "ja", className = "HanaScanRawQQ"),
+        SingleLang("Manhwa18", "https://manhwa18.com", "en", isNsfw = true, overrideVersionCode = 2),
+        SingleLang("Say Truyen", "https://saytruyen.net", "vi", overrideVersionCode = 2),
+        SingleLang("WeLoveManga", "https://weloma.art", "ja", pkgName = "rawlh", overrideVersionCode = 4),
     )
 
     companion object {
