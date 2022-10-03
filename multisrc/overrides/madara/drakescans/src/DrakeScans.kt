f6a916634bae095069bddb85a35a5d8a7818e39c
AntsyLich <59261191+AntsyLich@users.noreply.github.com>
2022-10-03 21:05:18 +0600

    Drake Scans: Remove tags from genre (#13696)

--- multisrc/overrides/madara/drakescans/src/DrakeScans.kt
+++ multisrc/overrides/madara/drakescans/src/DrakeScans.kt
@@ -2,4 +2,7 @@ package eu.kanade.tachiyomi.extension.en.drakescans
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class DrakeScans : Madara("Drake Scans", "https://drakescans.com", "en")
+class DrakeScans : Madara("Drake Scans", "https://drakescans.com", "en") {
+
+    override val mangaDetailsSelectorTag = ""
+}
