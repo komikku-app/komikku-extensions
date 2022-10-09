704f7bcd42dc0d065cc9a7319aa13df9aa3a9728
TheKingTermux <50316075+TheKingTermux@users.noreply.github.com>
2022-10-10 00:13:34 +0700

    ZManga: Add KomikIndo.info (#13779)
    * Add KomikIndo.info
    
    * Add KomikIndoInfo .kt file and Icons
    
    * Remove Series
    
    Making an error
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused icon
    
    * Remove unsused code
    
    * Fix missing spacing

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/zmanga/ZMangaGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/zmanga/ZMangaGenerator.kt
@@ -12,6 +12,7 @@ class ZMangaGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 1
 
     override val sources = listOf(
+        SingleLang("KomikIndo.info", "http://komikindo.info", "id", isNsfw = true, className = "KomikIndoInfo"),
         SingleLang("Komikita", "https://komikita.org", "id"),
         SingleLang("KomikPlay", "https://komikplay.com", "id", overrideVersionCode = 1),
         SingleLang("Maid - Manga", "https://www.maid.my.id", "id", overrideVersionCode = 10, className = "MaidManga"),
