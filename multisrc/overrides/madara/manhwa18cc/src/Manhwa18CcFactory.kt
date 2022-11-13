61b063a7c033944d223419f4f319fc4fd38a1762
Dexroneum <46288465+Dexroneum@users.noreply.github.com>
2022-11-13 19:43:48 +0000

    Manhwa18.cc little fixes (#14225)

--- multisrc/overrides/madara/manhwa18cc/src/Manhwa18CcFactory.kt
+++ multisrc/overrides/madara/manhwa18cc/src/Manhwa18CcFactory.kt
@@ -20,34 +20,36 @@ class Manhwa18CcFactory : SourceFactory {
     )
 }
 
-class Manhwa18CcEN : Manhwa18Cc("Manhwa18.cc", "https://manhwa18.cc", "en") {
-    override fun popularMangaSelector() = "div.manga-item:not(:contains(Raw))"
-}
-class Manhwa18CcKO : Manhwa18Cc("Manhwa18.cc", "https://manhwa18.cc", "ko") {
-    override fun popularMangaSelector() = "div.manga-item:contains(Raw)"
-}
 class Manhwa18CcALL : Manhwa18Cc("Manhwa18.cc", "https://manhwa18.cc", "all")
+
+class Manhwa18CcEN : Manhwa18Cc("Manhwa18.cc", "https://manhwa18.cc", "en") {
+    override fun popularMangaSelector() = "div.manga-item:not(:has(h3 a[title$='Raw']))"
+}
+
+class Manhwa18CcKO : Manhwa18Cc("Manhwa18.cc", "https://manhwa18.cc", "ko") {
+    override fun popularMangaSelector() = "div.manga-item:has(h3 a[title$='Raw'])"
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/raw/$page")
+}
 
 abstract class Manhwa18Cc(
     override val name: String,
     override val baseUrl: String,
-    lang: String
+    lang: String,
 ) : Madara(name, baseUrl, lang, SimpleDateFormat("dd MMM yyyy", Locale.US)) {
 
     override val useLoadMoreSearch = false
+
     override val fetchGenres = false
 
     override fun popularMangaSelector() = "div.manga-item"
-    override val popularMangaUrlSelector = "div.data > h3 > a"
-    override fun popularMangaNextPageSelector() = "ul.pagination > li > a"
 
-    override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl/webtoons/$page?orderby=trending")
-    }
+    override val popularMangaUrlSelector = "div.manga-item div.data a"
 
-    override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/webtoons/$page?orderby=latest")
-    }
+    override fun popularMangaNextPageSelector() = "ul.pagination li.next a"
+
+    override fun popularMangaRequest(page: Int): Request = GET("$baseUrl/webtoons/$page?orderby=trending")
+
+    override fun latestUpdatesRequest(page: Int): Request = GET("$baseUrl/webtoons/$page?orderby=latest")
 
     override fun searchMangaSelector() = popularMangaSelector()
 
@@ -58,22 +60,23 @@ abstract class Manhwa18Cc(
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         // After searching and go back to popular page, it always sent empty query thus display
         // "No results found" message. So this fix redirect to popular page.
-        if (query.isBlank())
-            return popularMangaRequest(1)
+        if (query.isBlank()) return popularMangaRequest(page)
 
         return GET("$baseUrl/search?q=$query&page=$page")
     }
 
     override val mangaSubString = "webtoon"
 
-    override fun chapterListSelector() = "li.wleft"
+    override val mangaDetailsSelectorDescription = "div.panel-story-description div.dsct"
+
+    override fun chapterListSelector() = "li.a-h"
 
     override fun chapterDateSelector() = "span.chapter-time"
 
     override val pageListParseSelector = "div.read-content img"
 
-    override fun pageListParse(document: Document): List<Page> {
-        return document.select(pageListParseSelector).mapIndexed { index, element ->
+    override fun pageListParse(document: Document): List<Page> = mutableListOf<Page>().apply {
+        document.select(pageListParseSelector).mapIndexed { index, element ->
             Page(
                 index,
                 document.location(),
