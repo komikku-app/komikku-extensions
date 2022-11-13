0a9555c292c406f155aa6cea37646994b6d3e25b
Luqman <riyanluqman@gmail.com>
2022-11-14 02:44:11 +0700

    Madara: fix lateinit not initialized for some source (#14228)
    * Madara: fix lateinit not initialized
    
    - check if initialized
    
    * Update multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>
    
    Co-authored-by: stevenyomi <95685115+stevenyomi@users.noreply.github.com>

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/Madara.kt
@@ -665,6 +665,12 @@ abstract class Madara(
                 .toMutableSet()
 
             // add tag(s) to genre
+            val mangaTitle = try {
+                manga.title
+            } catch (_: UninitializedPropertyAccessException) {
+                "not initialized"
+            }
+
             if (mangaDetailsSelectorTag.isNotEmpty()) {
                 select(mangaDetailsSelectorTag).forEach { element ->
                     if (genres.contains(element.text()).not() &&
@@ -672,7 +678,7 @@ abstract class Madara(
                         element.text().contains("read", true).not() &&
                         element.text().contains(name, true).not() &&
                         element.text().contains(name.replace(" ", ""), true).not() &&
-                        element.text().contains(manga.title, true).not() &&
+                        element.text().contains(mangaTitle, true).not() &&
                         element.text().contains(altName, true).not()
                     ) {
                         genres.add(element.text().lowercase(Locale.ROOT))
