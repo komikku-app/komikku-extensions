a556207c7a852901802283f44e1ef299692e3170
Tran Lam Nhat Nam <nhatnamleo@gmail.com>
2022-11-26 07:18:19 +0700

    Update nettruyen and nhattruyen to latest domain (#14378)
    * Changed domain nettruyen and nhattruyen
    
    * Change nettruyen and nhattruyen url

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
@@ -12,8 +12,8 @@ class WPComicsGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 2
 
     override val sources = listOf(
-        SingleLang("NetTruyen", "https://www.nettruyenon.com", "vi", overrideVersionCode = 11),
-        SingleLang("NhatTruyen", "https://nhattruyenon.com", "vi", overrideVersionCode = 9),
+        SingleLang("NetTruyen", "https://www.nettruyentv.com", "vi", overrideVersionCode = 12),
+        SingleLang("NhatTruyen", "https://nhattruyentv.com", "vi", overrideVersionCode = 10),
         SingleLang("TruyenChon", "http://truyenchon.com", "vi", overrideVersionCode = 3),
         SingleLang("XOXO Comics", "https://xoxocomics.com", "en", className = "XoxoComics", overrideVersionCode = 1),
     )
