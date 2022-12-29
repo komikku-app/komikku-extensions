4bb3a039a81da7b4384906722fec7167f5764297
mobi2002 <48650614+mobi2002@users.noreply.github.com>
2022-12-29 21:01:47 +0500

    ARESManga: fix 404 (#14720)

--- multisrc/overrides/mangathemesia/iimanga/src/ARESManga.kt
+++ multisrc/overrides/mangathemesia/iimanga/src/ARESManga.kt
@@ -4,7 +4,13 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class ARESManga : MangaThemesia("ARESManga", "https://aresmanga.net", "ar", dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("ar"))) {
+class ARESManga : MangaThemesia(
+    "ARESManga",
+    "https://aresmanga.net",
+    "ar",
+    mangaUrlDirectory = "/series",
+    dateFormat = SimpleDateFormat("MMMMM dd, yyyy", Locale("ar"))
+) {
     // The scanlator changed their name.
     override val id: Long = 230017529540228175
 }
