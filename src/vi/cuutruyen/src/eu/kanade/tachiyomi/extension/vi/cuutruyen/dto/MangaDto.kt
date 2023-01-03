5e9aca81b9144c858cd4cc494a69e97aa51ef5ea
beerpsi <92439990+beerpiss@users.noreply.github.com>
2023-01-03 21:00:48 +0700

    New source: Cứu Truyện (#14772)
    * New source: Cứu Truyện
    
    * chore: add more background info on `coordinates`
    
    * Add UrlActivity
    
    * more documentation on cuudrm_bg shenanigans
    
    * fix: close evaluation contexts to prevent memory leak
    
    * that wasnt needed

---
+++ src/vi/cuutruyen/src/eu/kanade/tachiyomi/extension/vi/cuutruyen/dto/MangaDto.kt
@@ -0,0 +1,53 @@
+package eu.kanade.tachiyomi.extension.vi.cuutruyen.dto
+
+import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthorDto(
+    val name: String,
+)
+
+@Serializable
+data class TeamDto(
+    val id: Int,
+    val name: String,
+    val description: String,
+)
+
+@Serializable
+data class MangaDto(
+    val id: Int,
+    val name: String,
+    @SerialName("cover_url") val coverUrl: String,
+    @SerialName("cover_mobile_url") val coverMobileUrl: String,
+
+    val author: AuthorDto? = null,
+    val author_name: String? = null,
+
+    val description: String? = null,
+    val team: TeamDto? = null,
+) {
+    fun toSManga(coverQuality: String? = null): SManga = SManga.create().apply {
+        val dto = this@MangaDto
+        url = "/mangas/${dto.id}"
+        title = dto.name
+        author = dto.author?.name ?: dto.author_name
+
+        description = ""
+        if (dto.team != null) {
+            description += "Nhóm dịch: ${dto.team.name}\n\n"
+        }
+        if (dto.description != null) {
+            description += dto.description
+        }
+
+        thumbnail_url = dto.coverUrl
+        if (coverQuality == "cover_url") {
+            thumbnail_url = dto.coverUrl
+        } else if (coverQuality == "cover_mobile_url") {
+            thumbnail_url = dto.coverMobileUrl
+        }
+    }
+}
---
+++ src/vi/cuutruyen/src/eu/kanade/tachiyomi/extension/vi/cuutruyen/dto/ResponseDto.kt
@@ -0,0 +1,18 @@
+package eu.kanade.tachiyomi.extension.vi.cuutruyen.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseDto<T>(
+    val data: T,
+    @SerialName("_metadata") val metadata: PaginationMetadataDto? = null
+)
+
+@Serializable
+data class PaginationMetadataDto(
+    @SerialName("total_count") val totalCount: Int,
+    @SerialName("total_pages") val totalPages: Int,
+    @SerialName("current_page") val currentPage: Int,
+    @SerialName("per_page") val perPage: Int,
+)
