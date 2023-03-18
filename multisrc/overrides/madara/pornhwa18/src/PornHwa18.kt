226d0d662c30879de89e37be9e3e1787fee36fea
arkon <eugcheung94@gmail.com>
2023-03-18 17:24:18 -0400

    Remove unnecessary override

--- multisrc/overrides/madara/pornhwa18/src/PornHwa18.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.id.pornhwa18
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class PornHwa18 : Madara("Pornhwa18", "https://pornhwa18.com", "id") {
-    override val versionId = 1
-}
