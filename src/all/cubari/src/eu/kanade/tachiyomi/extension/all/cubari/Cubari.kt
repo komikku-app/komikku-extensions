45553c5331a05eab257f4cbeb2ec9da16d2518b5
mobi2002 <48650614+mobi2002@users.noreply.github.com>
2023-03-24 07:37:24 +0500

    Cubari: implement client side search (#15803)

--- src/all/cubari/src/eu/kanade/tachiyomi/extension/all/cubari/Cubari.kt
+++ src/all/cubari/src/eu/kanade/tachiyomi/extension/all/cubari/Cubari.kt
@@ -224,17 +224,34 @@ open class Cubari(override val lang: String) : HttpSource() {
                 client.newBuilder()
                     .addInterceptor(RemoteStorageUtils.TagInterceptor())
                     .build()
-                    .newCall(searchMangaRequest(page, trimmedQuery, filters))
+                    .newCall(proxySearchRequest(trimmedQuery))
                     .asObservableSuccess()
                     .map { response ->
-                        searchMangaParse(response, trimmedQuery)
+                        proxySearchParse(response, trimmedQuery)
                     }
             }
-            else -> throw Exception(SEARCH_FALLBACK_MSG)
+            else -> {
+                client.newBuilder()
+                    .addInterceptor(RemoteStorageUtils.HomeInterceptor())
+                    .build()
+                    .newCall(searchMangaRequest(page, query, filters))
+                    .asObservableSuccess()
+                    .map { response ->
+                        searchMangaParse(response, query)
+                    }
+                    .map { mangasPage ->
+                        require(mangasPage.mangas.isNotEmpty()) { SEARCH_FALLBACK_MSG }
+                        mangasPage
+                    }
+            }
         }
     }
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+        return GET("$baseUrl/", headers)
+    }
+
+    private fun proxySearchRequest(query: String): Request {
         try {
             val queryFragments = query.split("/")
             val source = queryFragments[0]
@@ -251,6 +268,17 @@ open class Cubari(override val lang: String) : HttpSource() {
     }
 
     private fun searchMangaParse(response: Response, query: String): MangasPage {
+        val result = json.parseToJsonElement(response.body.string()).jsonArray
+
+        val filterList = result.asSequence()
+            .map { it as JsonObject }
+            .filter { it["title"].toString().contains(query.trim(), true) }
+            .toList()
+
+        return parseMangaList(JsonArray(filterList), SortType.ALL)
+    }
+
+    private fun proxySearchParse(response: Response, query: String): MangasPage {
         val result = json.parseToJsonElement(response.body.string()).jsonObject
         return parseSearchList(result, query)
     }
@@ -328,6 +356,8 @@ open class Cubari(override val lang: String) : HttpSource() {
                 parseManga(jsonObj)
             } else if (sortType == SortType.UNPINNED && !pinned) {
                 parseManga(jsonObj)
+            } else if (sortType == SortType.ALL) {
+                parseManga(jsonObj)
             } else {
                 null
             }
@@ -383,6 +413,7 @@ open class Cubari(override val lang: String) : HttpSource() {
         enum class SortType {
             PINNED,
             UNPINNED,
+            ALL,
         }
     }
 }
