73e38b0f3fff0fc6fee1fd83835b1c764f43cfb7
Eshlender <35057681+e-shl@users.noreply.github.com>
2023-04-06 17:21:05 +0500

    [RU]MangaPoisk new domain (#15971)

--- src/ru/mangapoisk/src/eu/kanade/tachiyomi/extension/ru/mangapoisk/MangaPoisk.kt
+++ src/ru/mangapoisk/src/eu/kanade/tachiyomi/extension/ru/mangapoisk/MangaPoisk.kt
@@ -23,7 +23,7 @@ import java.util.Locale
 class MangaPoisk : ParsedHttpSource() {
     override val name = "MangaPoisk"
 
-    override val baseUrl = "https://mangapoisk.ru"
+    override val baseUrl = "https://mangapoisk.com"
 
     override val lang = "ru"
 
