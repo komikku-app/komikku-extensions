1b14f9a9ecb4e492f3a4666e1645804b8c208542
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-04-17 15:45:30 -0300

    Remove Fire Mangás as its dead (#16082)
    Remove Fire Mangás as its dead.

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangasar/MangaSarGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangasar/MangaSarGenerator.kt
@@ -12,7 +12,6 @@ class MangaSarGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 7
 
     override val sources = listOf(
-        SingleLang("Fire Mangás", "https://firemangas.com", "pt-BR", className = "FireMangas"),
         SingleLang("Mangazim", "https://mangazim.com", "pt-BR"),
         SingleLang("MangásUp", "https://mangasup.net", "pt-BR", className = "MangasUp"),
         SingleLang("Seemangas", "https://seemangas.com", "pt-BR", isNsfw = true),
