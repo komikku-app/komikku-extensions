c6722d44b0e32bb3056f7f9f50884868541b2b1d
MeLoseAgain <116882702+MeLoseAgain@users.noreply.github.com>
2023-05-19 17:17:00 +0700

    Add Komikchan ID (#16408)
    Add Komik Chan

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/madara/MadaraGenerator.kt
@@ -169,6 +169,7 @@ class MadaraGenerator : ThemeSourceGenerator {
         SingleLang("Kissmanga.in", "https://kissmanga.in", "en", className = "KissmangaIn", overrideVersionCode = 3),
         SingleLang("KlikManga", "https://klikmanga.id", "id", overrideVersionCode = 2),
         SingleLang("KomikRame", "https://komikra.me", "id"),
+        SingleLang("Komik Chan", "https://komikchan.com", "id"),
         SingleLang("KSGroupScans", "https://ksgroupscans.com", "en"),
         SingleLang("Kun Manga", "https://kunmanga.com", "en", overrideVersionCode = 1),
         SingleLang("Lady Manga", "https://ladymanga.com", "en"),
