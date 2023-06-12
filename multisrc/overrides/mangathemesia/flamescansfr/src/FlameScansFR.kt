f1897430d6c9acec2bb4dc2bc275331ab8755286
KirinRaikage <manmitsu@hotmail.fr>
2023-06-12 22:21:53 +0200

    Add BananaScan and EpsilonScan and rename FlameScansFR to LegacyScans (#16713)
    * Add BananaScan and EpsilonScan to Mangathemesia and rename FlameScansFR to LegacyScans
    
    * Rename pkgName back to to FlameScansFr to avoid users having to migrate

--- multisrc/overrides/mangathemesia/flamescansfr/src/FlameScansFR.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.flamescansfr
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class FlameScansFR : MangaThemesia("FlameScans.fr", "https://flamescans.fr", "fr", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("fr")))
