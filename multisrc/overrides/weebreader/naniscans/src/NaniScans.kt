ebb44fffee4a686b700788155a6808308f2edd30
ObserverOfTime <chronobserver@disroot.org>
2023-06-25 05:09:58 +0300

    Weebreader: remove dead multisrc (#16847)

--- multisrc/overrides/weebreader/naniscans/src/NaniScans.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.naniscans
-
-import eu.kanade.tachiyomi.multisrc.weebreader.Weebreader
-
-class NaniScans : Weebreader("NANI? Scans", "https://naniscans.com", "en") {
-    override val versionId = 2
-}
