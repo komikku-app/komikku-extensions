ae20beba72f1cae05576596b661a4f9bd01a2125
KirinRaikage <manmitsu@hotmail.fr>
2023-06-25 04:09:28 +0200

    Add various French extensions to Madara (#16850)
    * Add various french extensions to Madara
    
    * Rename class and source to MangaHubFr to distinguish it better from OG MangaHub
    
    * Forgot comma to separate className and pkgName

---
+++ multisrc/overrides/madara/mangahubfr/src/MangaHubFr.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.fr.mangahubfr
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class MangaHubFr : Madara("MangaHub.fr", "https://mangahub.fr", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRENCH)) {
+    override fun chapterListSelector() = "li.wp-mangas-chapters"
+}
--- multisrc/overrides/madara/mangasorigines/src/MangasOrigines.kt
+++ multisrc/overrides/madara/mangasorigines/src/MangasOrigines.kt
@@ -1,94 +1,10 @@
 package eu.kanade.tachiyomi.extension.fr.mangasorigines
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SManga
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class MangasOrigines : Madara("Mangas Origines", "https://mangas-origines.fr", "fr", SimpleDateFormat("dd/MM/yyyy", Locale("fr"))) {
+class MangasOrigines : Madara("Mangas Origines", "https://mangas-origines.xyz", "fr", SimpleDateFormat("MMMM d, yyyy", Locale("fr"))) {
     override val useNewChapterEndpoint = true
-
-    private fun String.removeFireEmoji() = this.substringAfter("\uD83D\uDD25 ")
-
-    override fun popularMangaFromElement(element: Element): SManga {
-        return super.popularMangaFromElement(element).apply {
-            title = title.removeFireEmoji()
-        }
-    }
-
-    override fun mangaDetailsParse(document: Document): SManga {
-        val manga = SManga.create()
-        with(document) {
-            select("div.post-title h1").first()?.let {
-                manga.title = it.ownText().removeFireEmoji()
-            }
-            select("div.author-content > a").eachText().filter {
-                it.notUpdating()
-            }.joinToString().takeIf { it.isNotBlank() }?.let {
-                manga.author = it
-            }
-            select("div.artist-content > a").eachText().filter {
-                it.notUpdating()
-            }.joinToString().takeIf { it.isNotBlank() }?.let {
-                manga.artist = it
-            }
-            select("div.manga-excerpt").let {
-                if (it.select("p").text().isNotEmpty()) {
-                    manga.description = it.select("p").joinToString(separator = "\n\n") { p ->
-                        p.text().replace("<br>", "\n")
-                    }
-                } else {
-                    manga.description = it.text()
-                }
-            }
-            select("div.summary_image img").first()?.let {
-                manga.thumbnail_url = imageFromElement(it)
-            }
-            select("div.summary-content").last()?.let {
-                manga.status = with(it.text()) {
-                    when {
-                        // There's no support for LICENSED and PUBLISHING_FINISHED
-                        contains("Complété") -> SManga.COMPLETED
-                        contains("Annulé") -> SManga.CANCELLED
-                        contains("En pause") -> SManga.ON_HIATUS
-                        contains("En cours") -> SManga.ONGOING
-                        else -> SManga.UNKNOWN
-                    }
-                }
-            }
-            val genres = select("div.genres-content a")
-                .map { element -> element.text().lowercase(Locale.ROOT) }
-                .toMutableSet()
-
-            // add tag(s) to genre
-            select("div.tags-content a").forEach { element ->
-                if (genres.contains(element.text()).not()) {
-                    genres.add(element.text().lowercase(Locale.ROOT))
-                }
-            }
-
-            // add manga/manhwa/manhua thinggy to genre
-            document.select(seriesTypeSelector).firstOrNull()?.ownText()?.let {
-                if (it.isEmpty().not() && it.notUpdating() && it != "-" && genres.contains(it).not()) {
-                    genres.add(it.lowercase(Locale.ROOT))
-                }
-            }
-
-            manga.genre = genres.toList().joinToString(", ") { it.capitalize(Locale.ROOT) }
-
-            // add alternative name to manga description
-            document.select("div.summary-heading:contains(Autre) + .summary-content").firstOrNull()?.ownText()?.let {
-                if (it.isBlank().not() && it.notUpdating()) {
-                    manga.description = when {
-                        manga.description.isNullOrBlank() -> altName + it
-                        else -> manga.description + "\n\n$altName" + it
-                    }
-                }
-            }
-        }
-
-        return manga
-    }
+    override val mangaDetailsSelectorStatus = "div.summary-heading:contains(statut) + div.summary-content"
 }
