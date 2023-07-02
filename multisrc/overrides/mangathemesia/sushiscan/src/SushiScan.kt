8ad0505cd1d2bc0d44f535e5584e3a4e54648b29
KirinRaikage <manmitsu@hotmail.fr>
2023-07-02 22:38:53 +0200

    Fix status parsing for PhenixScans and SushiScan (#16988)

--- multisrc/overrides/mangathemesia/sushiscan/src/SushiScan.kt
+++ multisrc/overrides/mangathemesia/sushiscan/src/SushiScan.kt
@@ -11,14 +11,19 @@ import java.util.Locale
 
 class SushiScan : MangaThemesia("Sushi-Scan", "https://sushiscan.net", "fr", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.FRENCH)) {
     override val altNamePrefix = "Nom alternatif : "
-    override val seriesAuthorSelector = ".infotable tr:contains(Auteur) td:last-child"
-    override val seriesStatusSelector = ".infotable tr:contains(Statut) td:last-child"
+    override val seriesAuthorSelector = ".imptdt:contains(Auteur) i, .fmed b:contains(Auteur)+span"
+    override val seriesStatusSelector = ".imptdt:contains(Statut) i"
     override fun String?.parseStatus(): Int = when {
         this == null -> SManga.UNKNOWN
         this.contains("En Cours", ignoreCase = true) -> SManga.ONGOING
         this.contains("Terminé", ignoreCase = true) -> SManga.COMPLETED
         else -> SManga.UNKNOWN
     }
+
+    override fun mangaDetailsParse(document: Document): SManga =
+        super.mangaDetailsParse(document).apply {
+            status = document.select(seriesStatusSelector).text().parseStatus()
+        }
 
     // Overriding to fix http -> https when needed
     override fun pageListParse(document: Document): List<Page> {
