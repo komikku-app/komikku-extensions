226c64ceb8df6e67cdfd8d95fb0148cdf44f6c40
Rolando Lecca <90949336+bapeey@users.noreply.github.com>
2023-07-16 21:45:31 -0500

    Remove dead extensions (#17148)
    Remove dead sources

--- multisrc/overrides/mangathemesia/clayrer/src/Clayrer.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.es.clayrer
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Clayrer : MangaThemesia("Clayrer", "https://clayrer.net", "es", dateFormat = SimpleDateFormat("MMM d, yyyy", Locale("es")))
