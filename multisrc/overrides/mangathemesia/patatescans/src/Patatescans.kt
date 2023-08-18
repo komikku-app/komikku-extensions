c85a069ba3bcd2e147b40077c45dc834c45b1e7f
KirinRaikage <manmitsu@hotmail.fr>
2023-08-18 15:00:07 +0200

    Add Lelmanga and cleanup some dead sources in Mangathemesia (#17577)
    * Add Lelmanga and cleanup some dead sources in Mangathemesia
    
    * Update MangaThemesiaGenerator.kt

--- multisrc/overrides/mangathemesia/patatescans/src/Patatescans.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.patatescans
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class Patatescans : MangaThemesia("Patatescans", "https://patatescans.com", "fr")
