8d37adf0c2c83ca1dafd453dac80827e168b5ab7
KirinRaikage <manmitsu@hotmail.fr>
2023-08-20 17:23:06 +0200

    Add Shadowtrad and Mangas-Origines.fr and cleanup Madara sources (#17604)
    * Add Mangas-Origines.fr and Shadowtrad and cleanup multiple Madara sources
    
    * Remove AUTO GENERATE comments from Shadowtrad
    
    * Remove Log call from Shadowtrad

--- multisrc/overrides/madara/mangahubfr/src/MangaHubFr.kt
+++ multisrc/overrides/madara/mangahubfr/src/MangaHubFr.kt
@@ -4,6 +4,4 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class MangaHubFr : Madara("MangaHub.fr", "https://mangahub.fr", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRENCH)) {
-    override fun chapterListSelector() = "li.wp-mangas-chapters"
-}
+class MangaHubFr : Madara("MangaHub.fr", "https://mangahub.fr", "fr", dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.FRENCH))
--- multisrc/overrides/madara/mangasoriginesx/src/MangasOriginesX.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.fr.mangasoriginesx
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class MangasOriginesX : Madara("Mangas Origines X", "https://x.mangas-origines.fr", "fr", SimpleDateFormat("dd/MM/yyyy", Locale("fr")))
