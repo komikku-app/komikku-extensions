17378680dea0339e308b0e7b969ca0d78d90d921
bapeey <90949336+bapeey@users.noreply.github.com>
2023-08-26 09:24:38 -0500

    Add new sources to MangaThemesia (#17676)
    * Add new sources to MangaThemesia
    
    * DuniaKomikId -> Update Domain
    
    * Add ratelimit to SummerToon
    
    * Bump
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

--- multisrc/overrides/mangathemesia/duniakomikid/src/DuniaKomikId.kt
+++ multisrc/overrides/mangathemesia/duniakomikid/src/DuniaKomikId.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class DuniaKomikId : MangaThemesia("DuniaKomik.id", "https://duniakomik.id", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id", "ID"))) {
+class DuniaKomikId : MangaThemesia("DuniaKomik.id", "https://duniakomik.org", "id", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("id", "ID"))) {
 
     override val hasProjectPage = true
 }
