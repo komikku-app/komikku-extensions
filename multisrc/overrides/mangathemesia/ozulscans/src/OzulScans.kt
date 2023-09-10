7c194716b5374b6b07e3309230428e190586af24
Vetle Ledaal <vetle.ledaal@gmail.com>
2023-09-10 18:44:49 +0000

    Update domain for Ozul Scans (#17872)

--- multisrc/overrides/mangathemesia/ozulscans/src/OzulScans.kt
+++ multisrc/overrides/mangathemesia/ozulscans/src/OzulScans.kt
@@ -6,7 +6,7 @@ import java.util.Locale
 
 class OzulScans : MangaThemesia(
     "Ozul Scans",
-    "https://ozulscans.com",
+    "https://ozulscans.xyz",
     "ar",
     dateFormat = SimpleDateFormat("MMM d, yyy", Locale("ar")),
 )
