9ff49acf4b322f7886eb1b78e76bd1163b79876c
KirinRaikage <manmitsu@hotmail.fr>
2023-09-10 20:44:22 +0200

    Add Perf Scan to HeanCms (#17866)
    * Add Perf Scan to HeanCms
    
    * Remove blank lines in PerfScan.kt

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/heancms/HeanCmsGenerator.kt
@@ -14,6 +14,7 @@ class HeanCmsGenerator : ThemeSourceGenerator {
     override val sources = listOf(
         SingleLang("Glorious Scan", "https://gloriousscan.com", "pt-BR", overrideVersionCode = 17),
         SingleLang("Omega Scans", "https://omegascans.org", "en", isNsfw = true, overrideVersionCode = 17),
+        SingleLang("Perf Scan", "https://perf-scan.fr", "fr"),
         SingleLang("Reaper Scans", "https://reaperscans.net", "pt-BR", overrideVersionCode = 36),
         SingleLang("YugenMangas", "https://yugenmangas.net", "es", isNsfw = true, overrideVersionCode = 8),
     )
