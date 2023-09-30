e9bf94f4e57061e4230b9611a2cd5003cbe4be48
AhmadGhoni <93740880+AhmadGhoni@users.noreply.github.com>
2023-10-01 04:16:56 +0700

    MG Komik: Fix HTTP 404 error (#18276)
    Fix HTTP 404 error

--- multisrc/overrides/madara/mgkomik/src/MGKomik.kt
+++ multisrc/overrides/madara/mgkomik/src/MGKomik.kt
@@ -1,15 +1,32 @@
 package eu.kanade.tachiyomi.extension.id.mgkomik
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.network.GET
+import okhttp3.CacheControl
+import okhttp3.Request
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class MGKomik : Madara("MG Komik", "https://mgkomik.id", "id", SimpleDateFormat("dd MMM yy", Locale.US)) {
 
     override val chapterUrlSuffix = ""
-    override val mangaSubString = "komik"
 
     override fun searchMangaNextPageSelector() = "a.page.larger"
+    override fun popularMangaSelector() = searchMangaSelector()
 
-    override fun searchPage(page: Int): String = "halaman/$page/"
+    override fun popularMangaRequest(page: Int): Request {
+        return GET(
+            url = "$baseUrl/${searchPage(page)}?s&post_type=wp-manga&m_orderby=views",
+            headers = headers,
+            cache = CacheControl.FORCE_NETWORK,
+        )
+    }
+
+    override fun latestUpdatesRequest(page: Int): Request {
+        return GET(
+            url = "$baseUrl/${searchPage(page)}?s&post_type=wp-manga&m_orderby=new-manga",
+            headers = headers,
+            cache = CacheControl.FORCE_NETWORK,
+        )
+    }
 }
