673af3b8a628ccdf494b1e0a4542cb468d5a4494
Luqman <riyanluqman@gmail.com>
2023-10-17 05:18:41 +0700

    Aqua Manga: Fix Blocked by Cloudflare (#18564)

--- multisrc/overrides/madara/aquamanga/src/AquaManga.kt
+++ multisrc/overrides/madara/aquamanga/src/AquaManga.kt
@@ -11,10 +11,16 @@ class AquaManga : Madara("Aqua Manga", "https://aquamanga.com", "en") {
         .build()
 
     override fun headersBuilder(): Headers.Builder = super.headersBuilder()
-        .set("Origin", baseUrl)
-        .set("Sec-Fetch-Dest", "empty")
-        .set("Sec-Fetch-Mode", "cors")
-        .set("Sec-Fetch-Site", "same-origin")
+        .add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+        .add("Accept-Language", "en-US,en;q=0.5")
+        .add("DNT", "1")
+        .add("Referer", "$baseUrl/")
+        .add("Sec-Fetch-Dest", "document")
+        .add("Sec-Fetch-Mode", "navigate")
+        .add("Sec-Fetch-Site", "same-origin")
+        .add("Sec-Fetch-User", "?1")
+        .add("Upgrade-Insecure-Requests", "1")
+        .add("X-Requested-With", "XMLHttpRequest")
 
     override val chapterUrlSuffix = ""
 }
