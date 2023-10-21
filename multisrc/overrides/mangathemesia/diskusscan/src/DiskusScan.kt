dcd10c63f8f8cf61da3b625c0e261482eeadcfb7
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2023-10-21 13:38:31 -0300

    Fix some selectors in a MangaThemesia source (#18650)
    Fix some selectors in a MangaThemesia source.

--- multisrc/overrides/mangathemesia/diskusscan/src/DiskusScan.kt
+++ multisrc/overrides/mangathemesia/diskusscan/src/DiskusScan.kt
@@ -1,9 +1,13 @@
 package eu.kanade.tachiyomi.extension.pt.diskusscan
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
+import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Headers
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -19,10 +23,38 @@ class DiskusScan : MangaThemesia(
     override val versionId = 2
 
     override val client: OkHttpClient = super.client.newBuilder()
-        .rateLimit(1, 2, TimeUnit.SECONDS)
+        .rateLimit(2, 1, TimeUnit.SECONDS)
         .build()
 
     override fun headersBuilder(): Headers.Builder = super.headersBuilder()
-        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-        .set("Accept-Language", "pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3")
+        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+        .set("Accept-Language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6,gl;q=0.5")
+        .set("Dnt", "1")
+        .set("Sec-Fetch-Dest", "document")
+        .set("Sec-Fetch-Mode", "navigate")
+        .set("Sec-Fetch-Site", "same-origin")
+        .set("Sec-Fetch-User", "?1")
+
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        val newHeaders = headersBuilder()
+            .set("Referer", baseUrl + mangaUrlDirectory)
+            .build()
+
+        return GET(baseUrl + manga.url, newHeaders)
+    }
+
+    override val seriesAuthorSelector = ".infotable tr:contains(Autor) td:last-child"
+    override val seriesDescriptionSelector = ".entry-content[itemprop=description] > *:not([class^=disku])"
+
+    override fun chapterListRequest(manga: SManga) = mangaDetailsRequest(manga)
+
+    override fun imageUrlRequest(page: Page): Request {
+        val newHeaders = super.imageUrlRequest(page).headers.newBuilder()
+            .set("Sec-Fetch-Dest", "image")
+            .set("Sec-Fetch-Mode", "no-cors")
+            .set("Sec-Fetch-Site", "cross-site")
+            .build()
+
+        return GET(page.imageUrl!!, newHeaders)
+    }
 }
