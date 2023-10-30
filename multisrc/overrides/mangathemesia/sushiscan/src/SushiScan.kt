f2cc705cb775d63c0c0bca9b8a9b2089b36421ee
KirinRaikage <manmitsu@hotmail.fr>
2023-10-30 21:06:31 +0100

    Sushi-Scan: Add rate limit (#18794)

--- multisrc/overrides/mangathemesia/sushiscan/src/SushiScan.kt
+++ multisrc/overrides/mangathemesia/sushiscan/src/SushiScan.kt
@@ -1,15 +1,27 @@
 package eu.kanade.tachiyomi.extension.fr.sushiscan
 
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SManga
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
+import okhttp3.Headers
+import okhttp3.OkHttpClient
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 class SushiScan : MangaThemesia("Sushi-Scan", "https://sushiscan.net", "fr", mangaUrlDirectory = "/catalogue", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale.FRENCH)) {
+
+    override val client: OkHttpClient = super.client.newBuilder()
+        .rateLimit(2, 1, TimeUnit.SECONDS)
+        .build()
+
+    override fun headersBuilder(): Headers.Builder = super.headersBuilder()
+        .set("Referer", "$baseUrl$mangaUrlDirectory")
+
     override val altNamePrefix = "Nom alternatif : "
     override val seriesAuthorSelector = ".imptdt:contains(Auteur) i, .fmed b:contains(Auteur)+span"
     override val seriesStatusSelector = ".imptdt:contains(Statut) i"
--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangathemesia/MangaThemesiaGenerator.kt
@@ -133,7 +133,7 @@ class MangaThemesiaGenerator : ThemeSourceGenerator {
         SingleLang("Summer Fansub", "https://smmr.in", "pt-BR", isNsfw = true),
         SingleLang("SummerToon", "https://summertoon.com", "tr"),
         SingleLang("Surya Scans", "https://suryareader.com", "en", overrideVersionCode = 1),
-        SingleLang("Sushi-Scan", "https://sushiscan.net", "fr", className = "SushiScan", overrideVersionCode = 8),
+        SingleLang("Sushi-Scan", "https://sushiscan.net", "fr", className = "SushiScan", overrideVersionCode = 9),
         SingleLang("Sushiscan.fr", "https://sushiscan.fr", "fr", className = "SushiScanFR"),
         SingleLang("Tarot Scans", "https://www.tarotscans.com", "tr"),
         SingleLang("Tecno Scan", "https://tecnoscann.com", "es", isNsfw = true, overrideVersionCode = 6),
