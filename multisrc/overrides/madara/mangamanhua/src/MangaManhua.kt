f0ec2254c98f332e9d7cf7ef424e73bfb41dc7c6
Qlend <salemnabil1@outlook.com>
2023-11-21 01:32:21 +0300

    Remove dead sites and fix other (#19055)
    * remove dead sites and fix some
    
    * remove dead sites
    
    * remove space
    
    * edit

--- multisrc/overrides/madara/mangamanhua/src/MangaManhua.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.mangamanhua
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class MangaManhua : Madara("MangaManhua", "https://mangamanhua.online", "en", SimpleDateFormat("d MMMM'،' yyyy", Locale.US))
--- multisrc/overrides/mangathemesia/kingofshojo/src/KingofShojo.kt
+++ multisrc/overrides/mangathemesia/kingofshojo/src/KingofShojo.kt
@@ -4,7 +4,7 @@ import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class KingofShojo : MangaThemesia("King of Shojo", "https://kingofshojo.com", "ar", dateFormat = SimpleDateFormat("MMM d, yyy", Locale("ar"))) {
+class KingofShojo : MangaThemesia("King of Shojo", "https://kingofshojo.com", "ar", dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("ar"))) {
 
     override val hasProjectPage = true
 }
