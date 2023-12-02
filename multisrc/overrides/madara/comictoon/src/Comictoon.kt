a17eee9d8c553690053912acdffc08bad6f1f275
Vetle Ledaal <vetle.ledaal@gmail.com>
2023-12-02 15:41:06 +0000

    Remove more dead sources (#19157)
    * Remove Manga4All
    
    * Remove Hatachi Manga
    
    * Remove Comictoon
    
    * Remove Manga-Online.co
    
    * Remove Familiar by Soushiyo
    
    * Remove FaeStorm
    
    * Remove Painful Nightz Scan
    
    * Remove Scans Raw
    
    * Remove QManga
    
    * Remove MangaTigre

--- multisrc/overrides/madara/comictoon/src/Comictoon.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.th.comictoon
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
-
-class Comictoon : Madara("Comictoon", "https://comictoonthaith-new.com", "th", SimpleDateFormat("MMMMM dd, yyyy", Locale("th")))
--- multisrc/overrides/madara/scansraw/src/ScansRaw.kt
+++
@@ -1,7 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.scansraw
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class ScansRaw : Madara("Scans Raw", "https://scansraw.com", "en") {
-    override val useNewChapterEndpoint: Boolean = true
-}
