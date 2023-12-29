804c3ef7bc8ba54d1c963695bf9ab2835bd5a2bb
are-are-are <62763969+dejavui@users.noreply.github.com>
2023-12-29 22:17:23 +0700

    (multisrc/WPComic) Update domain Nettruyen & Nhattruyen (#19476)
    Update Domain Nettruyen & Nhattruyen

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/wpcomics/WPComicsGenerator.kt
@@ -12,8 +12,8 @@ class WPComicsGenerator : ThemeSourceGenerator {
     override val baseVersionCode: Int = 2
 
     override val sources = listOf(
-        SingleLang("NetTruyen", "https://www.nettruyenus.com", "vi", overrideVersionCode = 19),
-        SingleLang("NhatTruyen", "https://nhattruyenplus.com", "vi", overrideVersionCode = 12),
+        SingleLang("NetTruyen", "https://www.nettruyenbing.com", "vi", overrideVersionCode = 20),
+        SingleLang("NhatTruyen", "https://nhattruyenmax.com", "vi", overrideVersionCode = 13),
         SingleLang("TruyenChon", "http://truyenchon.com", "vi", overrideVersionCode = 3),
         SingleLang("XOXO Comics", "https://xoxocomics.net", "en", className = "XoxoComics", overrideVersionCode = 2),
     )
