70e8c747cbf9c9222dcbf5b3c42f7b5a2ea0b304
gimmeursocks <65237572+gimmeursocks@users.noreply.github.com>
2024-01-02 16:32:07 +0200

    (multisrc/a3manga) Updated a3manga domain (#19533)
    * fixing a3manga src link
    
    * baseVersionCode update

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/a3manga/A3MangaGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/a3manga/A3MangaGenerator.kt
@@ -9,10 +9,10 @@ class A3MangaGenerator : ThemeSourceGenerator {
 
     override val themeClass = "A3Manga"
 
-    override val baseVersionCode: Int = 2
+    override val baseVersionCode: Int = 3
 
     override val sources = listOf(
-        SingleLang("A3 Manga", "https://www.a3manga.info", "vi"),
+        SingleLang("A3 Manga", "https://www.a3manga.xyz", "vi"),
         SingleLang("Team Lanh Lung", "https://teamlanhlung.me", "vi", sourceName = "Team Lạnh Lùng", overrideVersionCode = 1),
         SingleLang("Ngon Phong", "https://www.ngonphong.com", "vi", sourceName = "Ngôn Phong", overrideVersionCode = 1),
         SingleLang("O Cu Meo", "https://www.ocumoe.com", "vi", sourceName = "Ổ Cú Mèo", overrideVersionCode = 1),
