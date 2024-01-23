7bc4146cc28bd55bda1cd694d6adf0ee96c112bd
Luqman <16263232+Riztard@users.noreply.github.com>
2024-01-23 12:03:45 +0700

    Clean Multisrc Generator (#504)
    clean generator

--- multisrc/overrides/madara/asurascansus/src/AsuraScansUs.kt
+++ multisrc/overrides/madara/asurascansus/src/AsuraScansUs.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.extension.en.asurascansus
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class AsuraScansUs : Madara("Asura Scans.us", "https://asurascans.us", "en") {
+    // Redirected from: https://mangagoyaoi.com
     override val useNewChapterEndpoint = true
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
