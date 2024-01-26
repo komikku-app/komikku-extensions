b0b0cb7020faa5e2385be411023aa0bb8d54b188
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-01-26 10:31:27 +0700

    Update name for MangaGoYaoi & TruyenChon to better showing that they are Dead

--- multisrc/overrides/wpcomics/truyenchon/src/TruyenChon.kt
+++ multisrc/overrides/wpcomics/truyenchon/src/TruyenChon.kt
@@ -11,7 +11,7 @@ import org.jsoup.nodes.Element
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class TruyenChon : WPComics("TruyenChon", "http://truyenchon.com", "vi", SimpleDateFormat("dd/MM/yy", Locale.getDefault()), null) {
+class TruyenChon : WPComics("TruyenChon (Dead)", "http://truyenchon.com", "vi", SimpleDateFormat("dd/MM/yy", Locale.getDefault()), null) {
     override val searchPath = "the-loai"
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
     override fun imageRequest(page: Page): Request = GET(page.imageUrl!!, headersBuilder().add("Referer", baseUrl).build())
