e6bc1f9cc009b0a02660231d13ffe608f38d194c
Cuong M. Tran <cuongtran.tm@gmail.com>
2024-01-26 16:18:47 +0700

    keep old MangaGo Yaoi ID since it might still working

--- multisrc/overrides/madara/mangagoyaoi/src/MangaGoYaoi.kt
+++ multisrc/overrides/madara/mangagoyaoi/src/MangaGoYaoi.kt
@@ -3,6 +3,8 @@ package eu.kanade.tachiyomi.extension.en.mangagoyaoi
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class MangaGoYaoi : Madara("MangaGo Yaoi (Moved)", "https://mangagoyaoi.com", "en") {
+    override val id = 5308594741304208652
+
     override val useNewChapterEndpoint = true
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
