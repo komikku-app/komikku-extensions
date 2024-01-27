673473751c99e04f3e76b6f855562fe5af51d38e
Mike <51273546+SnakeDoc83@users.noreply.github.com>
2024-01-26 21:05:19 -0500

    Remove MangaMainac (#664)

--- multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainacGenerator.kt
+++ multisrc/src/main/java/eu/kanade/tachiyomi/multisrc/mangamainac/MangaMainacGenerator.kt
@@ -1,6 +1,6 @@
 package eu.kanade.tachiyomi.multisrc.mangamainac
 
-import generator.ThemeSourceData.SingleLang
+import generator.ThemeSourceData
 import generator.ThemeSourceGenerator
 
 class MangaMainacGenerator : ThemeSourceGenerator {
@@ -11,22 +11,12 @@ class MangaMainacGenerator : ThemeSourceGenerator {
 
     override val baseVersionCode: Int = 1
 
-    override val sources = listOf(
-        SingleLang("Read Boku No Hero Academia Manga Online", "https://w23.readheroacademia.com/", "en"),
-        SingleLang("Read One Punch Man Manga Online", "https://w23.readonepunchman.net/", "en", overrideVersionCode = 1),
-        SingleLang("Read Solo Leveling", "https://mysololeveling.com/", "en", overrideVersionCode = 1, sourceName = "ReadSoloLeveling (Dead)"),
-        SingleLang("Read Berserk Manga Online", "https://berserkmanga.net/", "en"),
-        SingleLang("Read Domestic Girlfriend Manga", "https://domesticgirlfriend.net/", "en"),
-        SingleLang("Read Black Clover Manga", "https://w6.blackclovermanga2.com/", "en", overrideVersionCode = 1),
-        SingleLang("Read Shingeki no Kyojin Manga", "https://readshingekinokyojin.com/", "en"),
-        SingleLang("Read Nanatsu no Taizai Manga", "https://w2.readnanatsutaizai.net/", "en", overrideVersionCode = 1),
-        SingleLang("Read Rent a Girlfriend Manga", "https://kanojo-okarishimasu.com/", "en"),
-    )
+    override val sources = listOf<ThemeSourceData>()
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            MangaMainacGenerator().createAll()
+            // MangaMainacGenerator().createAll()
         }
     }
 }
