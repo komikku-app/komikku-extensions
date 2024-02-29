8ae8ec9381102757c49b625a69ec58e435d6341d
Cuong Tran <cuongtran.tm@gmail.com>
2024-03-01 02:05:17 +0700

    Photos18: search & translate tag/categories

--- src/all/photos18/src/eu/kanade/tachiyomi/extension/all/photos18/Photos18.kt
+++ src/all/photos18/src/eu/kanade/tachiyomi/extension/all/photos18/Photos18.kt
@@ -13,16 +13,18 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Headers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Document
-import org.jsoup.select.Evaluator
 import rx.Observable
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
+import java.net.URLDecoder
 
 class Photos18 : HttpSource(), ConfigurableSource {
     override val name = "Photos18"
@@ -36,44 +38,50 @@ class Photos18 : HttpSource(), ConfigurableSource {
 
     override val client = network.client.newBuilder().followRedirects(false).build()
 
-    override fun headersBuilder() = Headers.Builder().apply {
-        add("Referer", baseUrl)
-    }
+    override fun headersBuilder() = super.headersBuilder()
+        .add("Referer", "$baseUrl/")
 
-    override fun popularMangaRequest(page: Int) = GET("$baseUrlWithLang/sort/views?page=$page", headers)
+    override fun popularMangaRequest(page: Int) = GET("$baseUrlWithLang?sort=likes&page=$page", headers)
 
     override fun popularMangaParse(response: Response): MangasPage {
         val document = response.asJsoup()
-        parseCategories(document)
-        val mangas = document.selectFirst(Evaluator.Id("videos"))!!.children().map {
-            val cardBody = it.selectFirst(Evaluator.Class("card-body"))!!
-            val link = cardBody.selectFirst(Evaluator.Tag("a"))!!
+        val mangas = document.select("#videos div.card").map { card ->
+            val cardBody = card.select(".card-body")
+            val link = cardBody.select("a")
+            val category = cardBody.select("div label.badge").text()
             SManga.create().apply {
+                // stripLang() so both lang (traditional or simplified) will have same entries catalog
                 url = link.attr("href").stripLang()
-                title = link.ownText()
-                thumbnail_url = baseUrl + it.selectFirst(Evaluator.Tag("img"))!!.attr("src")
-                genre = cardBody.selectFirst(Evaluator.Tag("label"))!!.ownText()
+                title = link.text()
+                thumbnail_url = baseUrl + card.select("img").attr("src")
+                genre = translate(category)
+                description = category
                 status = SManga.COMPLETED
-                initialized = true
             }
         }
-        val isLastPage = document.selectFirst(Evaluator.Class("next")).run {
+        val isLastPage = document.selectFirst("nav .pagination .next").run {
             this == null || hasClass("disabled")
         }
         return MangasPage(mangas, !isLastPage)
     }
 
-    override fun latestUpdatesRequest(page: Int) = GET("$baseUrlWithLang/?page=$page", headers)
+    override fun latestUpdatesRequest(page: Int) = GET("$baseUrlWithLang/sort/created?page=$page", headers)
 
     override fun latestUpdatesParse(response: Response) = popularMangaParse(response)
 
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val url = baseUrlWithLang.toHttpUrl().newBuilder()
-            .addQueryParameter("q", query)
-            .addQueryParameter("page", page.toString())
+        if (query.isNotBlank()) {
+            url.addQueryParameter("q", query.trim())
+        }
+        url.addQueryParameter("page", page.toString())
 
-        for (filter in filters) {
-            if (filter is QueryFilter) filter.addQueryTo(url)
+        filters.forEach {
+            when (it) {
+                is KeywordFilter -> if (query.isBlank()) it.addQueryTo(url)
+                is QueryFilter -> it.addQueryTo(url)
+                else -> {}
+            }
         }
 
         return GET(url.build(), headers)
@@ -81,9 +89,26 @@ class Photos18 : HttpSource(), ConfigurableSource {
 
     override fun searchMangaParse(response: Response) = popularMangaParse(response)
 
-    override fun fetchMangaDetails(manga: SManga): Observable<SManga> = Observable.just(manga)
+    override fun mangaDetailsRequest(manga: SManga): Request {
+        return GET("$baseUrlWithLang${manga.url}", headers)
+    }
 
-    override fun mangaDetailsParse(response: Response) = throw UnsupportedOperationException()
+    override fun mangaDetailsParse(response: Response): SManga {
+        val document = response.asJsoup()
+        val category = document.select("nav li.breadcrumb-item:nth-child(2) a")
+
+        // Update entry's details in case language is switched
+        return SManga.create().apply {
+            title = document.select("title").text().trim()
+            thumbnail_url = document.select("div#content div.imgHolder")
+                .select("img").attr("src")
+            status = SManga.COMPLETED
+            if (category.toString().contains("href=\"(/zh-hans)?/cat/".toRegex())) {
+                genre = translate(category.text())
+                description = category.text()
+            }
+        }
+    }
 
     override fun fetchChapterList(manga: SManga): Observable<List<SChapter>> {
         val chapter = SChapter.create().apply {
@@ -98,7 +123,7 @@ class Photos18 : HttpSource(), ConfigurableSource {
 
     override fun pageListParse(response: Response): List<Page> {
         val document = response.asJsoup()
-        val images = document.selectFirst(Evaluator.Id("content"))!!.select(Evaluator.Tag("img"))
+        val images = document.select("div#content a img")
         return images.mapIndexed { index, image ->
             Page(index, imageUrl = image.attr("src"))
         }
@@ -106,14 +131,22 @@ class Photos18 : HttpSource(), ConfigurableSource {
 
     override fun imageUrlParse(response: Response) = throw UnsupportedOperationException()
 
-    override fun getFilterList() = FilterList(
-        SortFilter(),
-        if (categories.isEmpty()) {
-            Filter.Header("Tap 'Reset' to load categories")
-        } else {
-            CategoryFilter(categories)
-        },
-    )
+    override fun getFilterList(): FilterList {
+        launchIO { fetchCategories() }
+        return FilterList(
+            SortFilter(),
+            if (categoryList.isEmpty()) {
+                Filter.Header("Tap 'Reset' to load categories")
+            } else {
+                CategoryFilter(categoryList)
+            },
+            if (keywordList.isEmpty()) {
+                Filter.Header("Tap 'Reset' to load keywords")
+            } else {
+                KeywordFilter(keywordList)
+            },
+        )
+    }
 
     private open class QueryFilter(
         name: String,
@@ -131,34 +164,105 @@ class Photos18 : HttpSource(), ConfigurableSource {
         arrayOf("Latest", "Popular", "Trend", "Recommended", "Best"),
         "sort",
         arrayOf("created", "hits", "views", "score", "likes"),
-        state = 2,
+        state = 0,
     )
 
-    private class CategoryFilter(categories: List<Pair<String, String>>) : QueryFilter(
+    class Category(val name: String, val value: String)
+
+    private var categoryList: List<Category> = emptyList()
+
+    private class CategoryFilter(categories: List<Category>) : QueryFilter(
         "Category",
-        categories.map { it.first }.toTypedArray(),
+        categories.map { it.name }.toTypedArray(),
         "category_id",
-        categories.map { it.second }.toTypedArray(),
+        categories.map { it.value }.toTypedArray(),
     )
 
-    private var categories: List<Pair<String, String>> = emptyList()
+    /**
+     * Inner variable to control how much tries the categories request was called.
+     */
+    private var fetchCategoriesAttempts: Int = 0
 
-    private fun parseCategories(document: Document) {
-        if (categories.isNotEmpty()) return
-        val items = document.selectFirst(Evaluator.Id("w3"))!!.children()
-        categories = buildList(items.size + 1) {
-            add(Pair("All", ""))
-            items.mapTo(this) {
-                val value = it.text().substringBefore(" (")
-                val queryValue = it.selectFirst(Evaluator.Tag("a"))!!.attr("href").substringAfterLast('/')
-                Pair(value, queryValue)
+    /**
+     * Fetch the categories from the source to be used in the filters.
+     */
+    private fun fetchCategories() {
+        if (fetchCategoriesAttempts < 3 && categoryList.isEmpty()) {
+            try {
+                client.newCall(categoriesRequest()).execute()
+                    .let {
+                        val document = it.asJsoup()
+                        categoryList = parseCategories(document)
+                        keywordList = parseKeywords(document)
+                    }
+            } catch (_: Exception) {
+            } finally {
+                fetchCategoriesAttempts++
             }
         }
     }
 
-    private val preferences by lazy {
-        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)!!
+    /**
+     * The request to the search page (or another one) that have the categories list.
+     */
+    private fun categoriesRequest(): Request {
+        return GET("$baseUrlWithLang/node/keywords", headers)
     }
+
+    /**
+     * Get the categories from the search page document.
+     *
+     * @param document The search page document
+     */
+    private fun parseCategories(document: Document): List<Category> {
+        val items = document.select("#w2 a")
+        return buildList(items.size + 1) {
+            add(Category("All", ""))
+            items.mapTo(this) {
+                val value = it.text().substringBefore(" (").trim()
+                val queryValue = it.attr("href").substringAfterLast('/')
+                Category(translate(value), queryValue)
+            }
+        }
+    }
+
+    class Keyword(val name: String, val value: String)
+
+    private var keywordList: List<Keyword> = emptyList()
+
+    private class KeywordFilter(keywords: List<Keyword>) : QueryFilter(
+        "Keyword",
+        keywords.map { it.name }.toTypedArray(),
+        "q",
+        keywords.map { it.value }.toTypedArray(),
+    )
+
+    /**
+     * Get the keywords from the search page document.
+     *
+     * @param document The search page document
+     */
+    private fun parseKeywords(document: Document): List<Keyword> {
+        val items = document.select("div.content form#keywordForm ~ a.tag")
+        return buildList(items.size + 1) {
+            add(Keyword("None", ""))
+            items.mapTo(this) {
+                val value = it.text()
+                val queryValue = URLDecoder.decode(
+                    it.attr("href").substringAfterLast('/'),
+                    "UTF-8",
+                )
+                Keyword(translate(value), queryValue)
+            }
+        }
+    }
+
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    private fun launchIO(block: () -> Unit) = scope.launch { block() }
+
+    private val preferences =
+        Injekt.get<Application>().getSharedPreferences("source_$id", 0x0000)!!
 
     private val useTrad get() = preferences.getBoolean("ZH_HANT", false)
 
