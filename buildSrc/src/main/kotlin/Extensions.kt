32a57e676aa9a6c7d58e46d8cd7ce6a29dbe6757
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-01-12 12:43:13 +0500

    [ci] build only required modules (#6900)
    * workflow
    
    * configs
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * rewrite python scripts
    
    * small tweak
    
    * Update .github/scripts/modules-to-build.py
    
    Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
    
    * set full path of git
    
    * now?
    
    * i hate my life
    
    * and should work now
    
    * And some more changes
    
    * And some more changes 2
    
    * Oops
    
    * support for always building some modules
    
    * string replace fix
    
    * don't run matrix chunk if empty
    
    * bruh moment
    
    * revert
    
    * test pr workflow
    
    - remove extension
    - modify extension
    - modify multisrc
    
    * fix gradlew command
    
    * Revert "test pr workflow"
    
    This reverts commit 8c3aa34555a26f2257d2d791104eb63bbd48856c.
    
    * skip step
    
    * skip step x2
    
    * perhaps
    
    * make if work?
    
    * Reapply "test pr workflow"
    
    This reverts commit 9c7f2a7acd787b18b98ef99da733cc2f8077b2a7.
    
    * make if work!
    
    * Revert "Reapply "test pr workflow""
    
    This reverts commit 43238b0d7bfaa81b8067c7206bb9fb081fa4dbba.
    
    * update job name
    
    * Revert "update job name"
    
    This reverts commit 361b6e7279b901a78fac3eb77e909f480cf54475.
    
    * final changes
    
    * Cleanup some common code
    
    * Cleanup settings.gradle.kts
    
    * Reapply "Reapply "test pr workflow""
    
    This reverts commit 68524b649974e4d05b6a01d0ec297a5f28617071.
    
    * Revert "Reapply "Reapply "test pr workflow"""
    
    This reverts commit bafab8b8405cff9b9142d9f83c4f5adac92da6a0.
    
    * correct delete output name
    
    * pr test: multiple matrices
    
    * Revert "pr test: multiple matrices"
    
    This reverts commit 87fd5fcfd01dc4d83658312293fcc8ac3586086e.
    
    ---------
    
    Co-authored-by: FourTOne5 <107297513+FourTOne5@users.noreply.github.com>

--- buildSrc/src/main/kotlin/Extensions.kt
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -1,10 +1,40 @@
 import groovy.lang.MissingPropertyException
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.extra
 
 var ExtensionAware.baseVersionCode: Int
     get() = (extra.get("baseVersionCode") as Int) + kmkBaseVersionCode
     set(value) = extra.set("baseVersionCode", value)
+
+fun Project.getDependents(): Set<Project> {
+    val dependentProjects = mutableSetOf<Project>()
+
+    rootProject.allprojects.forEach { project ->
+        project.configurations.forEach { configuration ->
+            configuration.dependencies.forEach { dependency ->
+                if (dependency is ProjectDependency && dependency.path == path) {
+                    dependentProjects.add(project)
+                }
+            }
+        }
+    }
+
+    return dependentProjects
+}
+
+fun Project.printDependentExtensions() {
+    getDependents().forEach { project ->
+        if (project.path.startsWith(":src:")) {
+            println(project.path)
+        } else if (project.path.startsWith(":lib-multisrc:")) {
+            project.getDependents().forEach { println(it.path) }
+        } else if (project.path.startsWith(":lib:")) {
+            project.printDependentExtensions()
+        }
+    }
+}
 
 var ExtensionAware.kmkBaseVersionCode: Int
     get() {
